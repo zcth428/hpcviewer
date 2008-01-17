78eccd613db4a595fb7ef6e6685ce56717e27d0c
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2008-01-17 21:39:35 +0000

    use CDT editor for any files that will be used with external editors

--- main/src/edu/rice/cs/hpc/viewer/util/EditorManager.java
+++ main/src/edu/rice/cs/hpc/viewer/util/EditorManager.java
@@ -1,29 +1,60 @@
 package edu.rice.cs.hpc.viewer.util;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.IWorkbenchSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.part.FileEditorInput;
 
 import edu.rice.cs.hpc.data.experiment.scope.Scope;
 import edu.rice.cs.hpc.data.experiment.source.FileSystemSourceFile;
-import org.eclipse.ui.IWorkbenchWindow;
 
+/**
+ * Class specifically designed to manage editor such as displaying source code editor
+ * @author la5
+ *
+ */
 public class EditorManager {
     private IWorkbenchWindow windowCurrent;
 
+    /*
+    private void setDefaultEditor() {
+    	IEditorRegistry objRegistry;
+    	if(this.windowCurrent != null)
+    		objRegistry = this.windowCurrent.getWorkbench().getEditorRegistry();
+    	else
+    		objRegistry = PlatformUI.getWorkbench().getEditorRegistry();
+    	String sEditor = org.eclipse.ui.editors.text.TextEditor.class.toString();
+    	objRegistry.setDefaultEditor("*", sEditor);
+    	objRegistry.setDefaultEditor("*.f*", sEditor);
+    } */
     /**
      * 
      * @param window
      */
     public EditorManager(IWorkbenchWindow window) {
     	this.windowCurrent = window;
+    	//this.setDefaultEditor();
     }
     
 	public EditorManager(IWorkbenchSite site) {
 		this.windowCurrent = site.getWorkbenchWindow();
+    	//this.setDefaultEditor();
 	}
 	/**
 	 * Open and Display editor
@@ -84,7 +115,8 @@ public class EditorManager {
 	    		 return; // do we need this ?
 	    	}
 	    	try {
-	    		IEditorPart objEditor = org.eclipse.ui.ide.IDE.openEditorOnFileStore(wbPage, objFile);
+	    		IEditorPart objEditor = openEditorOnFileStore(wbPage, objFile); 
+	    			//org.eclipse.ui.ide.IDE.openEditorOnFileStore(wbPage, objFile);
 	    		/*IContentOutlinePage outlinePage = (IContentOutlinePage) objEditor.getAdapter(IContentOutlinePage.class);
 	    		 if (outlinePage != null) {
 	    		    // editor wishes to contribute outlinePage to content outline view
@@ -126,5 +158,114 @@ public class EditorManager {
 	       }
 
 	}
+
+	//-------========================= TAKEN FROM IDE ===============
+	/**
+	 * This is "home-made" method of IDE.openEditorOnFileStore since the IDE function
+	 * will use external editor on *nix machine for Fortran files !
+	 * @param page
+	 * @param fileStore
+	 * @return
+	 * @throws PartInitException
+	 */
+	public static IEditorPart openEditorOnFileStore(IWorkbenchPage page, IFileStore fileStore) throws PartInitException {
+        //sanity checks
+        if (page == null) {
+			throw new IllegalArgumentException();
+		}
+
+        IEditorInput input = getEditorInput(fileStore);
+        String editorId = getEditorId(fileStore);
+        //System.out.println("original editorID:"+ editorId);
+        if(editorId.compareTo(org.eclipse.ui.IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID) == 0) {
+        	editorId = "org.eclipse.cdt.ui.editor.CEditor";
+        	//editorId = "org.eclipse.ui.editors.text.TextEditor";
+        	//System.out.println("Disabling external editor, replace to internal editor:"+editorId);
+        }
+        // open the editor on the file
+        return page.openEditor(input, editorId);
+    }
+
+    /**
+     * Get the id of the editor associated with the given <code>IFileStore</code>.
+     * 
+	 * @param workbench
+	 * 	         the Workbench to use to determine the appropriate editor's id 
+     * @param fileStore
+     *           the <code>IFileStore</code> representing the file for which the editor id is desired
+	 * @return the id of the appropriate editor
+	 * @since 3.3
+	 */
+	private static String getEditorId(IFileStore fileStore) {
+		IEditorDescriptor descriptor;
+		try {
+			descriptor = IDE.getEditorDescriptor(fileStore.getName());
+		} catch (PartInitException e) {
+			return null;
+		}
+		if (descriptor != null)
+			return descriptor.getId();
+		return null;
+	}
+
+	/**
+	 * Create the Editor Input appropriate for the given <code>IFileStore</code>.
+	 * The result is a normal file editor input if the file exists in the
+	 * workspace and, if not, we create a wrapper capable of managing an
+	 * 'external' file using its <code>IFileStore</code>.
+	 * 
+	 * @param fileStore
+	 *            The file store to provide the editor input for
+	 * @return The editor input associated with the given file store
+	 */
+	private static IEditorInput getEditorInput(IFileStore fileStore) {
+		IFile workspaceFile = getWorkspaceFile(fileStore);
+		if (workspaceFile != null)
+			return new FileEditorInput(workspaceFile);
+		return new FileStoreEditorInput(fileStore);
+	}
+
+	/**
+	 * Determine whether or not the <code>IFileStore</code> represents a file
+	 * currently in the workspace.
+	 * 
+	 * @param fileStore
+	 *            The <code>IFileStore</code> to test
+	 * @return The workspace's <code>IFile</code> if it exists or
+	 *         <code>null</code> if not
+	 */
+	private static IFile getWorkspaceFile(IFileStore fileStore) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IFile[] files = root.findFilesForLocationURI(fileStore.toURI());
+		files = filterNonExistentFiles(files);
+		if (files == null || files.length == 0)
+			return null;
+
+		// for now only return the first file
+		return files[0];
+	}
+
+	/**
+	 * Filter the incoming array of <code>IFile</code> elements by removing
+	 * any that do not currently exist in the workspace.
+	 * 
+	 * @param files
+	 *            The array of <code>IFile</code> elements
+	 * @return The filtered array
+	 */
+	private static IFile[] filterNonExistentFiles(IFile[] files) {
+		if (files == null)
+			return null;
+
+		int length = files.length;
+		ArrayList<IFile> existentFiles = new ArrayList<IFile>(length);
+		for (int i = 0; i < length; i++) {
+			if (files[i].exists())
+				existentFiles.add(files[i]);
+		}
+		return (IFile[]) existentFiles.toArray(new IFile[existentFiles.size()]);
+	}
+	//-------========================= END TAKEN FROM IDE ===============
+
 
 }

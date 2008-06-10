ea0d98101f4af2d494965d302d973eedecc0ec95
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2008-06-10 17:50:20 +0000

    - fix issues on updating font for metric column by setting the font before inserting the text - bug fix: inserting the parent node on the top of the table when zoom out for the second time - removing unnecessary comment to avoid ambiguity

--- trunk/src/edu/rice/cs/hpc/data/experiment/scope/RootScope.java
+++ trunk/src/edu/rice/cs/hpc/data/experiment/scope/RootScope.java
@@ -118,32 +118,6 @@ public void accept(ScopeVisitor visitor, ScopeVisitType vt) {
 	 * recomputation of the flattening nodes
 	 */
 	private java.util.Hashtable<Integer, ArrayOfNodes> tableNodes;
-	/**
-	 * Method to retrieve the list of nodes 
-	 * @return
-	 */
-	/*
-	public java.util.Hashtable<Integer, ArrayOfNodes> getTableOfNodes() {
-		return this.tableNodes;
-	}*/
-	/**
-	 * method to generate the list of nodes
-	 * This method should be called when an experiment has been loaded
-	 */
-	/*
-	public void createFlattenNode() {
-		this.tableNodes =  new java.util.Hashtable<Integer, ArrayOfNodes>();
-		this.createFlattenNode(this.treeNode, 0, " ");
-	} */
-	
-	/**
-	 * variable to store the leaves in a given level
-	 * The list of the leaves is in form of ArrayOfNodes objects, which is an 
-	 *  array list of nodes
-	 */
-	//private java.util.Hashtable<Integer, ArrayOfNodes> tblLeaves = new java.util.Hashtable<Integer, ArrayOfNodes>();
-	//private java.util.ArrayList<Scope.Node[]> tblFlattenNodes;
-
 	
 	//=====================================
 	private void addChildren(Scope.Node node, ArrayOfNodes arrNodes) {
@@ -153,44 +127,6 @@ public void accept(ScopeVisitor visitor, ScopeVisitType vt) {
 		}
 	}
 
-	/**
-	 * Return the maximum depth of the tree
-	 * @return
-	 */
-	public int getMaxLevel() {
-		// the method getDepth is computed based on 1 as the root, while in
-		// our case, we prefer based on 0, so let decrease the value by 1
-		return this.getTreeNode().getDepth() - 1;
-	}
-	
-	/**
-	 * Return the list of flattened nodes
-	 * We will flatten the current node by one level
-	 * @return
-	 */
-	public ArrayOfNodes getFlatten() {
-		if(this.iFlattenLevel<0)
-			this.getFlatten(0);
-		return this.getFlatten(this.iFlattenLevel + 1);
-	}
-	
-	/**
-	 * Return the unflattened list of nodes
-	 * The tree has to be flattened, otherwise it return null
-	 * @return
-	 */
-	public ArrayOfNodes getUnflatten() {
-		return this.getFlatten(this.iFlattenLevel - 1);
-	}
-	
-	/**
-	 * Return the current level of flatten node
-	 * @return
-	 */
-	public int getFlattenLevel() {
-		return this.iFlattenLevel;
-	}
-	
 	/**
 	 * Return the list of flattened node
 	 * @param iLevel: level of flattened nodes, 0 is the root
@@ -243,47 +179,42 @@ public void accept(ScopeVisitor visitor, ScopeVisitType vt) {
 	}
 	//=====================================
 	/**
-	 * recursive private method to walk through the tree to get the list of nodes
-	 * @param node
-	 * @param iLevel
-	 * @param str
+	 * Return the maximum depth of the tree
+	 * @return
 	 */
-	/*
-	private void createFlattenNode(Scope.Node node, int iLevel, String str) {
-		if(node != null) {
-			Integer objLevel = Integer.valueOf(iLevel);
-			ArrayOfNodes listOfNodes ;
-			// verify if the list of nodes of this level already exists
-			if(this.tableNodes.containsKey(objLevel)) {
-				listOfNodes = this.tableNodes.get(objLevel);
-			} else 
-				listOfNodes = new ArrayOfNodes(iLevel);
-
-			// browse all the node's children
-			int nbChildren = node.getChildCount();
-			if(nbChildren>0){
-				for(int i=0;i<nbChildren;i++) {
-					Scope.Node nodeKid = (Scope.Node)node.getChildAt(i);
-					this.createFlattenNode(nodeKid, iLevel + 1, str + " ");
-				}
-			} else {
-				ArrayOfNodes listOfLeaves = tblLeaves.get(objLevel);
-				// this node is the leaf
-				if(listOfLeaves == null) {
-					listOfLeaves = new ArrayOfNodes(iLevel);
-				}
-				listOfLeaves.add(node);	// add this node to the list of leaves
-				this.tblLeaves.put(objLevel, listOfLeaves); // put it "back"
-			}
-			node.iLevel = iLevel;
-			// find the maximum level
-			if(this.MAX_LEVELS<iLevel) {
-				this.MAX_LEVELS = iLevel;
-			}
-			listOfNodes.add(node);
-			this.tableNodes.put(objLevel, listOfNodes);
-		}
+	public int getMaxLevel() {
+		// the method getDepth is computed based on 1 as the root, while in
+		// our case, we prefer based on 0, so let decrease the value by 1
+		return this.getTreeNode().getDepth() - 1;
 	}
-	*/
+	
+	/**
+	 * Return the list of flattened nodes
+	 * We will flatten the current node by one level
+	 * @return
+	 */
+	public ArrayOfNodes getFlatten() {
+		if(this.iFlattenLevel<0)
+			this.getFlatten(0);
+		return this.getFlatten(this.iFlattenLevel + 1);
+	}
+	
+	/**
+	 * Return the unflattened list of nodes
+	 * The tree has to be flattened, otherwise it return null
+	 * @return
+	 */
+	public ArrayOfNodes getUnflatten() {
+		return this.getFlatten(this.iFlattenLevel - 1);
+	}
+	
+	/**
+	 * Return the current level of flatten node
+	 * @return
+	 */
+	public int getFlattenLevel() {
+		return this.iFlattenLevel;
+	}
+	
 }
 

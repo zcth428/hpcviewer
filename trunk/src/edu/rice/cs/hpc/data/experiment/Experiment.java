cc30477bd00519a88accec903202abe4ff710143
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2008-09-30 19:10:54 +0000

    - Remove Inclusive/Exclusive facility from creating a derived metrics - Clean up the code, add some comments

--- trunk/src/edu/rice/cs/hpc/data/experiment/Experiment.java
+++ trunk/src/edu/rice/cs/hpc/data/experiment/Experiment.java
@@ -265,25 +265,7 @@ protected void accumulateMetricsFromKids(Scope target, Scope source, MetricValue
 		target.accumulateMetrics(child, filter, this.getMetricCount());
 	}
 }
-/*
-protected void accumulateMetricsFromKidsNoRecursive(Scope target, Scope source, MetricValuePropagationFilter filter) {
-	int nkids = source.getSubscopeCount();
-	for (int i = 0; i < nkids; i++) {
-		Scope child = source.getSubscope(i);
-		if (child instanceof LoopScope) {
-			accumulateMetricsFromKidsNoRecursive(target, child, filter);
-		}
-		if(target instanceof ProcedureScope && source instanceof ProcedureScope) {
-			ProcedureScope objTarget = (ProcedureScope) target;
-			ProcedureScope objSource = (ProcedureScope) source;
-			if(objTarget.getSID() != objSource.getSID())
-				target.accumulateMetrics(child, filter, this.getMetricCount());
-			else
-				System.out.println("E:"+objTarget.getSID()+"\t"+objSource.getSID());
-		}
-	}
-}
-*/
+
 protected void copyMetric(Scope target, Scope source, int src_i, int targ_i, MetricValuePropagationFilter filter) {
 	if (filter.doPropagation(source, target, src_i, targ_i)) {
 		MetricValue mv = source.getMetricValue(src_i);
@@ -316,7 +298,6 @@ protected Scope createCallersView(Scope callingContextViewRootScope)
 	callingContextViewRootScope.dfsVisitScopeTree(csv);
 	// compute the aggregate metrics
 	accumulateMetricsFromKids(callersViewRootScope, callersViewRootScope, filter);
-	//accumulateMetricsFromKidsNoRecursive(callersViewRootScope, callersViewRootScope, filter);
 	return callersViewRootScope;
 }
 

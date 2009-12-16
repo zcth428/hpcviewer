e8f2ab8475d5fac89c6b41356d1b533c9d921006
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2009-12-16 00:17:15 +0000

    - add product definition for galileo (we need to start with it, it's the default eclipse to download now) - bug fix: force aggregate metric to be the type DERIVED_INCR

--- branches/sam/src/edu/rice/cs/hpc/data/experiment/metric/AggregateMetric.java
+++ branches/sam/src/edu/rice/cs/hpc/data/experiment/metric/AggregateMetric.java
@@ -23,6 +23,14 @@ public class AggregateMetric extends BaseMetric {
 	// map variable 
 	private MetricVarMap varMap;
 
+	/***
+	 * Constructor: create a derived incremental metric
+	 * @param sID: the ID of the metric (HAS TO BE REALLY SHORT !!!)
+	 * @param sDisplayName: the title of the metric
+	 * @param displayed: to show or not ?
+	 * @param percent  : to show the percent ?
+	 * @param index    : metric index in the list (unused)
+	 */
 	public AggregateMetric(String sID, String sDisplayName, boolean displayed,
 			boolean percent, int index) {
 		super(sID, sDisplayName, displayed, percent, index);
@@ -32,28 +40,23 @@ public class AggregateMetric extends BaseMetric {
 		
 		// set up the variables
 		this.varMap = new MetricVarMap();
-
+		// laksono hack 2009: we force this class of type DERIVED_INCR ... yuck !
+		this.metricType = MetricType.DERIVED_INCR;
 	}
 
 	@Override
 	public MetricValue getValue(Scope s) {
-		//assert ( this.formulaCurrent != null );
-		
 		MetricValue mv = null;
-		/*
-		this.varMap.setScope(s);
-		try {
-			double dValue = this.formulaCurrent.eval(this.varMap, this.fctMap);
-			mv = new MetricValue(dValue);
-		} catch(java.lang.Exception e) {
-			mv = MetricValue.NONE;
-		}
-		*/
 		mv = s.getMetricValue(this.index);
 		return mv;
 	}
 
-	
+
+	/**
+	 * set the math expression
+	 * @param type
+	 * @param sFormula
+	 */
 	public void setFormula(char type, String sFormula) {
 		assert (type == FORMULA_COMBINE || type == FORMULA_FINALIZE);
 		
@@ -69,10 +72,22 @@ public class AggregateMetric extends BaseMetric {
 	}
 	
 	
+	/***
+	 * initialize the metric.
+	 * THIS METHOD HAS TO BE CALLED before asking the value
+	 * @param type
+	 * @param exp
+	 */
 	public void init(char type, Experiment exp) {
 		this.varMap.setxperiment(exp);
 	}
 	
+	
+	/**
+	 * Assign the value of a scope based on the formula of a given type
+	 * @param type
+	 * @param scope
+	 */
 	public void setScopeValue(char type, Scope scope) {
 		Expression exp;
 		

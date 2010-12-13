349bf18e766a4dc6b202aceb1eaeab67274b2dc8
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2010-12-13 19:36:11 +0000

    Temporary hack to solve handling hexadecimal metric value for hpctoolkit IBS version. The problem is that the metric value is stored as "double" while java "%x" format only accepts either integer or double. The ideal is to have a universal metric value type. But I am afraid we will run out of time for the release. Thus, the temporary solution is to check if we need a hexadecimal formal or not. If this is the case, we convert from double into long (integer seems to small)

--- trunk/src/edu/rice/cs/hpc/data/experiment/metric/MetricValuePredefinedFormat.java
+++ trunk/src/edu/rice/cs/hpc/data/experiment/metric/MetricValuePredefinedFormat.java
@@ -2,6 +2,12 @@ package edu.rice.cs.hpc.data.experiment.metric;
 
 import java.util.Formatter;
 
+
+/**********************************************************
+ * 
+ * @author laksonoadhianto
+ *
+ **********************************************************/
 public class MetricValuePredefinedFormat implements IMetricValueFormat {
 	private String format;
 	
@@ -12,12 +18,33 @@ public class MetricValuePredefinedFormat implements IMetricValueFormat {
 	
 	public String format(MetricValue value) {
 		Formatter format_str = new Formatter();
-		format_str = format_str.format(format, value.value);
-		if (format_str != null) {
-			return format_str.toString();
-		} else {
-			return "";
+		try {
+			// --------------------------------------------------------------------------------
+			// temporary bug fix: if the format is hexadecimal (%x), the input value
+			//  has to be either an integer or a long number. Since we store by default
+			//	the value to be "double", we should convert it to long (integer is too small)
+			// 
+			// for next release, we need to have a universal metric value type that can 
+			//	represent any possible value such as double, int, long, hex, ...
+			// --------------------------------------------------------------------------------
+			if (format.indexOf("%x")>=0) {
+				long valong = (long) value.getValue();
+				format_str = format_str.format(format, valong);
+				
+			} else {
+				format_str = format_str.format(format, value.getValue());
+			}
+
+			if (format_str != null) {
+				return format_str.toString();
+			} else {
+				return "";
+			}
+		} catch (java.util.IllegalFormatConversionException e) {
+			System.err.println("Illegal format conversion: " + format.toString() + "\tFrom value: " + value.getValue());
+			e.printStackTrace();
 		}
+		return "";
 	}
 
 }

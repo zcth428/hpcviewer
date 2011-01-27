6e4b5ac118dcaf8a9f19154732e2a0280cf20a96
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-01-27 22:34:32 +0000

    Forced merging hpcviewer and hpctraceviewer via svn (since Eclipse is not good enough to force-merging) - The trunk repository now hosts three plugins:   - edu.rice.cs.hpc.data as the kernel plugin   - edu.rice.cs.hpc.viewer as hpcviewer application   - edu.rice.cs.hpc.traceviewer as hpctraceviewer application
    In order to check-out hpcviewer and hpctraceviewer, we need to check-out from fresh the trunk and select "find projects in the children of the selected resource".
    
    Thanks to eclipse, the svn update may not work well to anticipate sub-project.

---
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/framework/HPCDepthView.java
@@ -0,0 +1,125 @@
+package edu.rice.cs.hpc.traceviewer.framework;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+import edu.rice.cs.hpc.traceviewer.painter.DepthTimeCanvas;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
+
+public class HPCDepthView extends ViewPart
+{
+	public static final String ID = "hpcdepthview.view";
+	
+	/**The composite that holds everything in the view*/
+	Composite master;
+	
+	/**The max depth of the space time data.*/
+	int maxDepth;
+	
+	/**The selectedProcess from the detail canvas/view*/
+	int process;
+	
+	/** Stores/Creates all of the data that is used in the view. */
+	SpaceTimeData stData;
+	
+	/** Paints and displays the detail view. */
+	DepthTimeCanvas depthCanvas;
+	
+	/** Determines whether this view has been setup.*/
+	boolean initialized = false;
+	
+	/** Determines whether this view has been setup.*/
+	//public boolean openedView = false;
+	
+	HPCTraceView traceview;
+	
+	HPCCallStackView csview;
+	
+	public void createPartControl(Composite _master)
+	{
+		master = _master;
+		
+		try 
+		{
+			traceview = (HPCTraceView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(HPCTraceView.ID);
+		}
+		catch (PartInitException e) 
+		{
+			traceview = null;
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		setupEverything();
+	}
+	
+	public void setupEverything()
+	{
+		process = traceview.getSelectedProcess();
+		if(process==-1)
+			process = 0;
+		
+		stData = traceview.getData();
+		maxDepth = stData.getMaxDepth();
+		
+		/*************************************************************************
+		 * Master Composite
+		 */
+		
+		master.setLayout(new GridLayout());
+		master.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		/*************************************************************************
+		 * Depth View Canvas
+		 */
+		
+		depthCanvas = new DepthTimeCanvas(master, maxDepth, stData, traceview.detailCanvas, process);
+		depthCanvas.setLayout(new GridLayout());
+		depthCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		/**************************************************************************
+         * Time dimension labels
+         *
+        Group labelGroup = new Group(master, SWT.EMBEDDED);
+        GridLayout labelGroupLayout = new GridLayout();
+        labelGroupLayout.numColumns = 3;
+        labelGroup.setLayout(labelGroupLayout);
+        labelGroup.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+
+        depthCanvas.setLabels(labelGroup);*/
+	}
+
+	public void setFocus()
+	{
+		if (initialized)
+		{
+			//openedView = true;
+			depthCanvas.setCSSample();
+		}
+	}
+	
+	public void updateProcess()
+	{
+		process = traceview.getSelectedProcess();
+		if (process == -1)
+			process = 0;
+		if (process!=depthCanvas.process)
+		{
+			depthCanvas.process = process;
+			depthCanvas.home();
+		}
+	}
+	
+	public void setCSView(HPCCallStackView _csview)
+	{
+		csview = _csview;
+		depthCanvas.csViewer = csview.csViewer;
+		traceview.detailCanvas.setDepthCanvas(depthCanvas);
+		initialized = true;
+	}
+}
---
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/framework/HPCTraceView.java
@@ -0,0 +1,423 @@
+package edu.rice.cs.hpc.traceviewer.framework;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.ui.part.ViewPart;
+
+import edu.rice.cs.hpc.traceviewer.painter.SpaceTimeDetailCanvas;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
+
+/**A view for displaying the traceviewer.*/
+//all the GUI setup for the detail view is here
+public class HPCTraceView extends ViewPart
+{
+	
+	/**The ID needed to create this view (used in plugin.xml).*/
+	public static final String ID = "hpctraceview.view";
+	
+	/** Stores/Creates all of the data that is used in the view.*/
+	SpaceTimeData stData;
+	
+	/** Paints and displays the detail view.*/
+	SpaceTimeDetailCanvas detailCanvas;
+	
+	/** Determines whether this view has been setup.*/
+	boolean initialized = false;
+	
+	/** Stores the current depth that is being displayed.*/
+	int currentDepth;
+	
+	HPCDepthView depthview;
+	
+	HPCCallStackView csview;
+	
+	/*************************************************************************
+	 *	Creates the view.
+	 ************************************************************************/
+	//TO THE NEXT SCHLIMAZEL THAT WORKS ON THIS - THIS METHOD IS WHERE EVERYTHING STARTS
+	public void createPartControl(Composite master)
+	{
+		setupEverything(master);
+	}
+	
+	/**************************************************************************
+	 * Sets up everything that is to be displayed on the view.
+	 *************************************************************************/
+	public void setupEverything(final Composite master)
+	{
+		DirectoryDialog dialog;
+		ArrayList<File> traceFiles = null;
+		File experimentFile = null;
+
+		boolean validDatabaseFound = false;
+		dialog = new DirectoryDialog(master.getShell());
+		dialog.setMessage("Please select the directory which holds the trace databases.");
+		dialog.setText("Select Data Directory");
+		String dir;
+		while(!validDatabaseFound)
+		{
+			traceFiles = new ArrayList<File>();
+			
+			dir = dialog.open();
+			
+			if (dir == null) master.getShell().close();
+			File dirFile = new File(dir);
+			String[] databases = dirFile.list();
+			if ((databases == null)) master.getShell().close();
+			
+			experimentFile = new File(dir+File.separatorChar+"experiment.xml");
+			
+			for (int databaseId = 0; databaseId < databases.length; databaseId++)
+			{
+				String cstName = dir+File.separatorChar+databases[databaseId];
+				if (cstName.contains(".hpctrace"))
+				{
+					traceFiles.add(new File(dir+File.separatorChar+databases[databaseId]));
+					validDatabaseFound = true;
+				}
+			}
+			if (!experimentFile.exists())
+				validDatabaseFound = false;
+			
+			if (!validDatabaseFound)
+				dialog.setMessage("The directory selected contains no trace databases.\n" +
+						"Please select the directory which holds the trace databases.");
+		}
+		
+		stData = new SpaceTimeData(master, experimentFile, traceFiles);
+		currentDepth = 0;
+		int maxDepth = stData.getMaxDepth();
+
+		/*************************************************************************
+		 * Master Composite
+		 ************************************************************************/
+		
+		master.setLayout(new GridLayout());
+		master.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		/***************************************************
+		 * Buttons
+		 **************************************************/
+		Group buttonGroup = new Group(master, SWT.EMBEDDED);
+		GridLayout buttonLayout = new GridLayout();
+		buttonLayout.numColumns = 9;
+		buttonLayout.makeColumnsEqualWidth = true;
+		
+		// tighten layout -- johnmc
+		buttonLayout.marginHeight = 0;
+		buttonLayout.verticalSpacing = 0;
+		buttonLayout.marginWidth = 0;
+		buttonLayout.verticalSpacing = 0;
+		buttonLayout.horizontalSpacing = 0;
+	
+		buttonGroup.setLayout(buttonLayout);
+		buttonGroup.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
+		
+		Button home = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor homeSamp = ImageDescriptor.createFromFile(this.getClass(), "home-screen.png");
+		Image homeScreen = homeSamp.createImage();
+		home.setImage(homeScreen);
+		home.setToolTipText("Resets the view to the home screen");
+		home.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.home();
+			}
+		});
+		home.setEnabled(true);
+		
+		
+		Button tZoomIn = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor zoomInTim = ImageDescriptor.createFromFile(this.getClass(), "zoom-in-time.png");
+		Image zoomInTime = zoomInTim.createImage();
+		tZoomIn.setImage(zoomInTime);
+		tZoomIn.setToolTipText("Zoom in on the time axis");
+		tZoomIn.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.timeZoomIn();
+			}
+		});
+		tZoomIn.setEnabled(true);
+
+		Button tZoomOut = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor zoomOutTim = ImageDescriptor.createFromFile(this.getClass(), "zoom-out-time.png");
+		Image zoomOutTime = zoomOutTim.createImage();
+		tZoomOut.setImage(zoomOutTime);
+		tZoomOut.setToolTipText("Zoom out on the time axis");
+		tZoomOut.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.timeZoomOut();
+			}
+		});
+		tZoomOut.setEnabled(false);
+
+		Button pZoomIn = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor zoomInProc = ImageDescriptor.createFromFile(this.getClass(), "zoom-in-process.png");
+		Image zoomInProcess = zoomInProc.createImage();
+		pZoomIn.setImage(zoomInProcess);
+		pZoomIn.setToolTipText("Zoom in on the process axis");
+		pZoomIn.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.processZoomIn();
+			}
+		});
+		pZoomIn.setEnabled(true);
+
+		Button pZoomOut = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor zoomOutProc = ImageDescriptor.createFromFile(this.getClass(), "zoom-out-process.png");
+		Image zoomOutProcess = zoomOutProc.createImage();
+		pZoomOut.setImage(zoomOutProcess);
+		pZoomOut.setToolTipText("Zoom out on the process axis");
+		pZoomOut.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.processZoomOut();
+			}
+		});
+		pZoomOut.setEnabled(false);
+		
+
+		Button undo = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor undoSamp = ImageDescriptor.createFromFile(this.getClass(), "undo.png");
+		Image undoArrow = undoSamp.createImage();
+		undo.setImage(undoArrow);
+		undo.setToolTipText("Undo the last made action");
+		undo.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.popUndo();
+				if (detailCanvas.getDepth() != currentDepth)
+				{
+					csview.depthEditor.setSelection(detailCanvas.getDepth());
+					detailCanvas.popUndo();
+				}
+			}
+		});
+		undo.setEnabled(false);
+		
+		Button redo = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor redoSamp = ImageDescriptor.createFromFile(this.getClass(), "redo.png");
+		Image redoArrow = redoSamp.createImage();
+		redo.setImage(redoArrow);
+		redo.setToolTipText("Redo the last undo");
+		redo.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				detailCanvas.popRedo();
+				if (detailCanvas.getDepth() != currentDepth)
+				{
+					csview.depthEditor.setSelection(detailCanvas.getDepth());
+					detailCanvas.popRedo();
+				}
+			}
+		});
+		redo.setEnabled(false);
+		
+	
+		
+		Button save = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor saveSamp = ImageDescriptor.createFromFile(this.getClass(), "save.png");
+		Image saveImage = saveSamp.createImage();
+		save.setImage(saveImage);
+		save.setToolTipText("Save the view configuration to a file");
+		save.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				FileDialog saveDialog;
+				saveDialog = new FileDialog(master.getShell(), SWT.SAVE);
+				saveDialog.setText("Save View Configuration");
+				String fileName = "";
+				boolean validSaveFileFound = false;
+				while(!validSaveFileFound)
+				{
+					Frame toSave = detailCanvas.save();
+					saveDialog.setFileName((int)toSave.begTime+"-"+(int)toSave.endTime+", "
+						+(int)toSave.begProcess+"-"+(int)toSave.endProcess+".bin");
+					fileName = saveDialog.open();
+					
+					if (fileName == null)
+						return;
+					else
+					{
+						if (!new File(fileName).exists())
+							validSaveFileFound = true;
+						else
+						{
+							MessageBox msg = new MessageBox(master.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+							msg.setText("File Exists");
+							msg.setMessage("This file path already exists.\nDo you want to overwrite this save file?");
+							int selectionChoice = msg.open();
+							if (selectionChoice==SWT.YES)
+								validSaveFileFound = true;
+							else
+								validSaveFileFound = false;
+							//open message box confirming whether or not they want to overwrite saved file
+							//if they select yes, validSaveFileFound = true;
+							//if they selct no, validSaveFileFound = false;
+						}
+					}
+				}
+				
+				try
+				{
+					ObjectOutputStream out = null;
+					try
+					{
+						out = new ObjectOutputStream(new FileOutputStream(fileName));
+						out.writeObject(detailCanvas.save());
+					}
+					finally
+					{
+						out.close();
+					}
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+		save.setEnabled(true);
+		
+		
+
+		Button open = new Button(buttonGroup, SWT.PUSH);
+		ImageDescriptor openSamp = ImageDescriptor.createFromFile(this.getClass(), "open.png");
+		Image openImage = openSamp.createImage();
+		open.setImage(openImage);
+		open.setToolTipText("Open a saved view configuration");
+		open.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event)
+			{
+				FileDialog openDialog;
+				openDialog = new FileDialog(master.getShell(), SWT.OPEN);
+				openDialog.setText("Open View Configuration");
+				String fileName = "";
+				boolean validFrameFound = false;
+				while(!validFrameFound)
+				{
+					fileName = openDialog.open();
+					
+					if (fileName == null) return;
+					File binFile = new File(fileName);
+					
+					if (binFile.exists())
+					{
+						try
+						{
+							ObjectInputStream in = null;
+							try
+							{
+								in = new ObjectInputStream(new FileInputStream(fileName));
+								Frame current = (Frame)in.readObject();
+								detailCanvas.open(current);
+								validFrameFound = true;
+							}
+							finally
+							{
+								in.close();
+							}
+						}
+						catch (IOException e)
+						{
+							validFrameFound = false;
+						}
+						catch (ClassNotFoundException e)
+						{
+							validFrameFound = false;
+						}
+					}
+				}
+			}
+		});
+		open.setEnabled(true);
+		
+		/*************************************************************************
+		 * Detail View Canvas
+		 ************************************************************************/
+		
+		detailCanvas = new SpaceTimeDetailCanvas(master, maxDepth, stData);
+		detailCanvas.setDepth(currentDepth);
+		detailCanvas.setLayout(new GridLayout());
+		detailCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		detailCanvas.setButtons(new Button[]{home,open,save,undo,redo,tZoomIn,tZoomOut,pZoomIn,pZoomOut});
+		
+		/**************************************************************************
+         * Process and Time dimension labels
+         *************************************************************************/
+		Composite labelGroup = new Composite(master, SWT.EMBEDDED);
+        GridLayout labelGroupLayout = new GridLayout();
+        labelGroupLayout.numColumns = 3;
+        
+		// tighten layout -- johnmc
+        labelGroupLayout.marginHeight = 1;
+        labelGroupLayout.verticalSpacing = 0;
+        labelGroupLayout.verticalSpacing = 0;
+	
+        labelGroup.setLayout(labelGroupLayout);
+        labelGroup.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+
+        //detailCanvas takes care of updating the labels
+        detailCanvas.setLabels(labelGroup);    
+	}
+
+	/*************************************************************************
+	 *	Updates/sets the depth that is displayed in the context view and 
+	 *	detail view.
+	 ************************************************************************/
+	public void setDepth(int depth, boolean textBox)
+	{
+		currentDepth = depth;
+		detailCanvas.setDepth(depth);
+		csview.csViewer.setDepth(depth);
+		this.stData.getDepthTimeCanvas().setDepth(depth);
+		if(textBox)
+			csview.depthEditor.setSelection(depth);
+	}
+
+	/**Required in order to extend ViewPart.*/
+	public void setFocus()
+	{
+		if (initialized)
+			detailCanvas.setCSSample();
+	}
+	
+	public SpaceTimeData getData()
+	{
+		return stData;
+	}
+	
+	public int getSelectedProcess()
+	{
+		return detailCanvas.selectedProcess;
+	}
+	
+	public void setCSView(HPCCallStackView _csview)
+	{
+		csview = _csview;
+		detailCanvas.csViewer = csview.csViewer;
+		initialized = true;
+	}
+}

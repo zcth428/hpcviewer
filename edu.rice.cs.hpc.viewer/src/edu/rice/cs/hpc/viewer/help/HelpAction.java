596d2ac4e1fd98dcb7cc8e652643bf1cb1d8aa70
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-02-15 19:02:32 +0000

    Bug fix: hpcviewer help is not displayed: when user select "hpcviewer help" menu, an error message appears ".... not found"
    The cause of this is that after the merge, the full path of the html file is not the "root workspace" but "plugin bundle" which is:
     <project>/edu.rice.cs.hpc.viewer/...  
    and as exported product:
    <plugins>/edu.rice.cs.hpc.viewer_release_number/...  
    
    The solution is to recover the path of the plug-in and the bundle, and reconstruct the full path of the resource for html file

--- edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/help/HelpAction.java
+++ edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/help/HelpAction.java
@@ -3,20 +3,22 @@
  */
 package edu.rice.cs.hpc.viewer.help;
 
-import org.eclipse.ui.part.FileEditorInput;
-import org.eclipse.ui.PartInitException;
+import java.io.IOException;
+import java.net.URL;
 
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchPage;
 
-//import edu.rice.cs.hpc.viewer.util.EditorManager;
 
 /**
  * @author laksonoadhianto
@@ -24,6 +26,7 @@ import org.eclipse.ui.IWorkbenchPage;
  */
 public class HelpAction extends Action {
 	static public String ID="edu.rice.cs.hpc.viewer.help.HelpAction";
+	final static private String HELP_FILE_PATH = "/doc/hpcviewer-users-manual.html";
 	protected IWorkbenchWindow objWindow;
 	
 	public HelpAction(IWorkbenchWindow window) {
@@ -37,18 +40,45 @@ public class HelpAction extends Action {
 		
 		IWorkspace objWorkspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = objWorkspace.getRoot();
-		//java.net.URL objURL = this.getClass().getResource("hpcviewer.html");
-		
-		//Path location = new Path(objURL.getFile());
-		Path location = new Path("doc/hpcviewer-users-manual.html");
-		IFile file = root.getFile(location);
+
+		String sFilePath = this.getHelpPath();
+		IFile file = root.getFile(new Path(sFilePath));
 		
 		IWorkbenchPage objPage = this.objWindow.getActivePage(); 
-		FileEditorInput objInput = new FileEditorInput((IFile)file);
+				
 		try {
+			FileEditorInput objInput = new FileEditorInput(file);
 			objPage.openEditor(objInput, HTMLEditor.ID);
-		} catch (PartInitException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+	
+
+	
+	/****
+	 * retrieve help path of the local resource
+	 * @return
+	 */
+	private String getHelpPath() {
+		// URL to the root ("/") of the plugin-path:
+		URL relativeURL = Platform.getBundle("edu.rice.cs.hpc.viewer").getEntry(HELP_FILE_PATH);
+
+		// Turn relative path to a local path with the help of Eclipse-platform:
+		URL localURL;
+		try {
+			localURL = FileLocator.resolve(relativeURL);
+
+			// From this you can get the path
+			String pluginDirString = localURL.getPath();
+			
+			return pluginDirString;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }

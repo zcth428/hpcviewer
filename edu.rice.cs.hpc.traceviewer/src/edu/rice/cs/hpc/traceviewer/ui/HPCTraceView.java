355d5c5b23fadba8f83ac697dd750a0ad5dbbb10
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-03-08 18:44:56 +0000

    Rearrange the toolbar and refactoring spaghetti codes. Instead of mixing actions and the ui, we attempt to separate them to make it clearer. However, the code still needs major surgery. In order to be really clean it requires humongous efforts.

--- edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/ui/HPCTraceView.java
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/ui/HPCTraceView.java
@@ -9,20 +9,12 @@ import java.io.IOException;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.CoolBar;
-import org.eclipse.swt.widgets.CoolItem;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.part.ViewPart;
 
@@ -34,7 +26,7 @@ import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
 
 /**A view for displaying the traceviewer.*/
 //all the GUI setup for the detail view is here
-public class HPCTraceView extends ViewPart implements ITraceDepth, ITracePosition
+public class HPCTraceView extends ViewPart implements ITraceDepth, ITracePosition, ITraceViewAction
 {
 	
 	/**The ID needed to create this view (used in plugin.xml).*/
@@ -51,7 +43,7 @@ public class HPCTraceView extends ViewPart implements ITraceDepth, ITracePositio
 	
 	private HPCCallStackView csview;
 	
-	private Composite coolBarArea;
+	private TraceCoolBar coolBarArea;
 	
 	/*************************************************************************
 	 *	Creates the view.
@@ -131,300 +123,14 @@ public class HPCTraceView extends ViewPart implements ITraceDepth, ITracePositio
 	
 	
 	private void createToolbar(Composite parent) {
-		coolBarArea = new Composite(parent, SWT.NONE);
-		final CoolBar coolBar = new CoolBar(coolBarArea, SWT.NONE);
-		final ToolBar toolBar = new ToolBar(coolBar, SWT.FLAT);
-		
-		/***************************************************
-		 * Buttons
-		 **************************************************/
+		coolBarArea = new TraceCoolBar(parent, this, SWT.NONE);
 
-		final ToolItem home = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor homeSamp = ImageDescriptor.createFromFile(this.getClass(), "home-screen.png");
-		Image homeScreen = homeSamp.createImage();
-		home.setImage(homeScreen);
-		home.setToolTipText("Resets the view to the home screen");
-		home.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.home();
-			}
-		});
-		home.setEnabled(true);
-		
-		new ToolItem(toolBar, SWT.SEPARATOR);
-		
-		final ToolItem tZoomIn = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor zoomInTim = ImageDescriptor.createFromFile(this.getClass(), "zoom-in-time.png");
-		Image zoomInTime = zoomInTim.createImage();
-		tZoomIn.setImage(zoomInTime);
-		tZoomIn.setToolTipText("Zoom in on the time axis");
-		tZoomIn.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.timeZoomIn();
-			}
-		});
-		tZoomIn.setEnabled(true);
-
-		final ToolItem tZoomOut = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor zoomOutTim = ImageDescriptor.createFromFile(this.getClass(), "zoom-out-time.png");
-		Image zoomOutTime = zoomOutTim.createImage();
-		tZoomOut.setImage(zoomOutTime);
-		tZoomOut.setToolTipText("Zoom out on the time axis");
-		tZoomOut.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.timeZoomOut();
-			}
-		});
-		tZoomOut.setEnabled(false);
-
-		final ToolItem pZoomIn = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor zoomInProc = ImageDescriptor.createFromFile(this.getClass(), "zoom-in-process.png");
-		Image zoomInProcess = zoomInProc.createImage();
-		pZoomIn.setImage(zoomInProcess);
-		pZoomIn.setToolTipText("Zoom in on the process axis");
-		pZoomIn.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.processZoomIn();
-			}
-		});
-		pZoomIn.setEnabled(true);
-
-		final ToolItem pZoomOut = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor zoomOutProc = ImageDescriptor.createFromFile(this.getClass(), "zoom-out-process.png");
-		Image zoomOutProcess = zoomOutProc.createImage();
-		pZoomOut.setImage(zoomOutProcess);
-		pZoomOut.setToolTipText("Zoom out on the process axis");
-		pZoomOut.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.processZoomOut();
-			}
-		});
-		pZoomOut.setEnabled(false);
-				
-		new ToolItem(toolBar, SWT.SEPARATOR);
-
-		final ToolItem undo = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor undoSamp = ImageDescriptor.createFromFile(this.getClass(), "undo.png");
-		Image undoArrow = undoSamp.createImage();
-		undo.setImage(undoArrow);
-		undo.setToolTipText("Undo the last made action");
-		undo.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.popUndo();
-				if (detailCanvas.getDepth() != stData.getDepth())
-				{
-					csview.depthEditor.setSelection(detailCanvas.getDepth());
-					detailCanvas.popUndo();
-				}
-			}
-		});
-		undo.setEnabled(false);
-		
-		final ToolItem redo = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor redoSamp = ImageDescriptor.createFromFile(this.getClass(), "redo.png");
-		Image redoArrow = redoSamp.createImage();
-		redo.setImage(redoArrow);
-		redo.setToolTipText("Redo the last undo");
-		redo.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				detailCanvas.popRedo();
-				if (detailCanvas.getDepth() != stData.getDepth())
-				{
-					csview.depthEditor.setSelection(detailCanvas.getDepth());
-					detailCanvas.popRedo();
-				}
-			}
-		});
-		redo.setEnabled(false);
-				
-		new ToolItem(toolBar, SWT.SEPARATOR);
-		
-		final ToolItem save = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor saveSamp = ImageDescriptor.createFromFile(this.getClass(), "save.png");
-		Image saveImage = saveSamp.createImage();
-		save.setImage(saveImage);
-		save.setToolTipText("Save the view configuration to a file");
-		save.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				FileDialog saveDialog;
-				saveDialog = new FileDialog(coolBar.getShell(), SWT.SAVE);
-				saveDialog.setText("Save View Configuration");
-				String fileName = "";
-				boolean validSaveFileFound = false;
-				while(!validSaveFileFound)
-				{
-					Frame toSave = detailCanvas.save();
-					saveDialog.setFileName((int)toSave.begTime+"-"+(int)toSave.endTime+", "
-						+(int)toSave.begProcess+"-"+(int)toSave.endProcess+".bin");
-					fileName = saveDialog.open();
-					
-					if (fileName == null)
-						return;
-					else
-					{
-						if (!new File(fileName).exists())
-							validSaveFileFound = true;
-						else
-						{
-							MessageBox msg = new MessageBox(coolBar.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-							msg.setText("File Exists");
-							msg.setMessage("This file path already exists.\nDo you want to overwrite this save file?");
-							int selectionChoice = msg.open();
-							if (selectionChoice==SWT.YES)
-								validSaveFileFound = true;
-							else
-								validSaveFileFound = false;
-							//open message box confirming whether or not they want to overwrite saved file
-							//if they select yes, validSaveFileFound = true;
-							//if they selct no, validSaveFileFound = false;
-						}
-					}
-				}
-				
-				try
-				{
-					ObjectOutputStream out = null;
-					try
-					{
-						out = new ObjectOutputStream(new FileOutputStream(fileName));
-						out.writeObject(detailCanvas.save());
-					}
-					finally
-					{
-						out.close();
-					}
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
-		save.setEnabled(true);
-
-		final ToolItem open = new ToolItem(toolBar, SWT.PUSH);
-		ImageDescriptor openSamp = ImageDescriptor.createFromFile(this.getClass(), "open.png");
-		Image openImage = openSamp.createImage();
-		open.setImage(openImage);
-		open.setToolTipText("Open a saved view configuration");
-		open.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event)
-			{
-				FileDialog openDialog;
-				openDialog = new FileDialog(coolBar.getShell(), SWT.OPEN);
-				openDialog.setText("Open View Configuration");
-				String fileName = "";
-				boolean validFrameFound = false;
-				while(!validFrameFound)
-				{
-					fileName = openDialog.open();
-					
-					if (fileName == null) return;
-					File binFile = new File(fileName);
-					
-					if (binFile.exists())
-					{
-						try
-						{
-							ObjectInputStream in = null;
-							try
-							{
-								in = new ObjectInputStream(new FileInputStream(fileName));
-								Frame current = (Frame)in.readObject();
-								detailCanvas.open(current);
-								validFrameFound = true;
-							}
-							finally
-							{
-								in.close();
-							}
-						}
-						catch (IOException e)
-						{
-							validFrameFound = false;
-						}
-						catch (ClassNotFoundException e)
-						{
-							validFrameFound = false;
-						}
-					}
-				}
-			}
-		});
-		open.setEnabled(true);
-		
-		final ToolItem goEast = new ToolItem(toolBar, SWT.PUSH);
-		final ImageDescriptor eastDesc = ImageDescriptor.createFromFile(getClass(), "go-east.png");
-		goEast.setImage(eastDesc.createImage());
-		goEast.setToolTipText("Move left one step");
-		goEast.addListener(SWT.Selection, new Listener(){
-
-			public void handleEvent(Event event) {
-				detailCanvas.goEast();
-			}
-			
-		});
-		
-		final ToolItem goWest = new ToolItem(toolBar, SWT.PUSH);
-		final ImageDescriptor westDesc = ImageDescriptor.createFromFile(getClass(), "go-west.png");
-		goWest.setImage(westDesc.createImage());
-		goWest.setToolTipText("Move right one step");
-		goWest.addListener(SWT.Selection, new Listener(){
-
-			public void handleEvent(Event event) {
-				detailCanvas.goWest();
-			}
-			
-		});
-		
-		final ToolItem goNorth = new ToolItem(toolBar, SWT.PUSH);
-		final ImageDescriptor northDesc = ImageDescriptor.createFromFile(getClass(), "go-north.png");
-		goNorth.setImage(northDesc.createImage());
-		goNorth.setToolTipText("Move up one step");
-		goNorth.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event event) {
-				detailCanvas.goNorth();
-			}
-			
-		});
-		
-		final ToolItem goSouth = new ToolItem(toolBar, SWT.PUSH);
-		final ImageDescriptor southDesc = ImageDescriptor.createFromFile(getClass(), "go-south.png");
-		goSouth.setImage(southDesc.createImage());
-		goSouth.setToolTipText("Move down one step");
-		goSouth.addListener(SWT.Selection, new Listener(){
-			public void handleEvent(Event event) {
-				detailCanvas.goSouth();
-			}
-			
-		});
-		
-		GridLayoutFactory.fillDefaults().extendedMargins(5, 0, 0, 0).numColumns(1).generateLayout(coolBar);
-		GridDataFactory.fillDefaults().applyTo(coolBar);
-		
-		new ToolItem(toolBar, SWT.SEPARATOR);
-
-		toolBar.pack();
-		Point size = toolBar.getSize();
-		
-		final CoolItem buttonItems = new CoolItem(coolBar, SWT.NONE);
-		buttonItems.setControl(toolBar);
-		Point preferred = buttonItems.computeSize(size.x, size.y);
-		buttonItems.setPreferredSize(preferred);
-		
 		/**************************************************************************
          * Process and Time dimension labels
          *************************************************************************/
-		Composite labelGroup = new Composite(coolBarArea, SWT.NONE);
-		
+		final Composite labelGroup = new Composite(coolBarArea, SWT.NONE);
+		labelGroup.setBackground(this.getSite().getShell().getDisplay().getSystemColor(SWT.COLOR_DARK_YELLOW));
+				
 		/*************************************************************************
 		 * Detail View Canvas
 		 ************************************************************************/
@@ -438,9 +144,163 @@ public class HPCTraceView extends ViewPart implements ITraceDepth, ITracePositio
 		GridLayoutFactory.swtDefaults().numColumns(2).generateLayout(coolBarArea);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(coolBarArea);
 
-		detailCanvas.setButtons(new ToolItem[]{home,open,save,undo,redo,tZoomIn,tZoomOut,pZoomIn,pZoomOut});
+		detailCanvas.setButtons(new ToolItem[]{coolBarArea.home, coolBarArea.open, coolBarArea.save, coolBarArea.undo,
+				coolBarArea.redo, coolBarArea.tZoomIn, coolBarArea.tZoomOut, coolBarArea.pZoomIn, coolBarArea.pZoomOut});
 		
 		detailCanvas.setVisible(false);
 		coolBarArea.setVisible(false);
 	}
+
+	public void home() {
+		detailCanvas.home();
+	}
+
+	public void timeZoomIn() {
+		detailCanvas.timeZoomIn();
+	}
+
+	public void timeZoomOut() {
+		detailCanvas.timeZoomOut();
+	}
+
+	public void processZoomIn() {
+		detailCanvas.processZoomIn();
+	}
+
+	public void processZoomOut() {
+		detailCanvas.processZoomOut();
+	}
+
+	public void undo() {
+		detailCanvas.popUndo();
+		if (detailCanvas.getDepth() != stData.getDepth())
+		{
+			csview.depthEditor.setSelection(detailCanvas.getDepth());
+			detailCanvas.popUndo();
+		}
+	}
+
+	public void redo() {
+		detailCanvas.popRedo();
+		if (detailCanvas.getDepth() != stData.getDepth())
+		{
+			csview.depthEditor.setSelection(detailCanvas.getDepth());
+			detailCanvas.popRedo();
+		}
+	}
+	
+	public void save() {
+		FileDialog saveDialog;
+		saveDialog = new FileDialog(this.getViewSite().getShell(), SWT.SAVE);
+		saveDialog.setText("Save View Configuration");
+		String fileName = "";
+		boolean validSaveFileFound = false;
+		while(!validSaveFileFound)
+		{
+			Frame toSave = detailCanvas.save();
+			saveDialog.setFileName((int)toSave.begTime+"-"+(int)toSave.endTime+", "
+				+(int)toSave.begProcess+"-"+(int)toSave.endProcess+".bin");
+			fileName = saveDialog.open();
+			
+			if (fileName == null)
+				return;
+			else
+			{
+				if (!new File(fileName).exists())
+					validSaveFileFound = true;
+				else
+				{
+					MessageBox msg = new MessageBox(this.getViewSite().getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+					msg.setText("File Exists");
+					msg.setMessage("This file path already exists.\nDo you want to overwrite this save file?");
+					int selectionChoice = msg.open();
+					if (selectionChoice==SWT.YES)
+						validSaveFileFound = true;
+					else
+						validSaveFileFound = false;
+					//open message box confirming whether or not they want to overwrite saved file
+					//if they select yes, validSaveFileFound = true;
+					//if they selct no, validSaveFileFound = false;
+				}
+			}
+		}
+		
+		try
+		{
+			ObjectOutputStream out = null;
+			try
+			{
+				out = new ObjectOutputStream(new FileOutputStream(fileName));
+				out.writeObject(detailCanvas.save());
+			}
+			finally
+			{
+				out.close();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	public void open() {
+		FileDialog openDialog;
+		openDialog = new FileDialog(this.getViewSite().getShell(), SWT.OPEN);
+		openDialog.setText("Open View Configuration");
+		String fileName = "";
+		boolean validFrameFound = false;
+		while(!validFrameFound)
+		{
+			fileName = openDialog.open();
+			
+			if (fileName == null) return;
+			File binFile = new File(fileName);
+			
+			if (binFile.exists())
+			{
+				try
+				{
+					ObjectInputStream in = null;
+					try
+					{
+						in = new ObjectInputStream(new FileInputStream(fileName));
+						Frame current = (Frame)in.readObject();
+						detailCanvas.open(current);
+						validFrameFound = true;
+					}
+					finally
+					{
+						in.close();
+					}
+				}
+				catch (IOException e)
+				{
+					validFrameFound = false;
+				}
+				catch (ClassNotFoundException e)
+				{
+					validFrameFound = false;
+				}
+			}
+		}
+	}
+
+	public void goNorth() {
+		detailCanvas.goNorth();
+	}
+
+	public void goSouth() {
+		detailCanvas.goSouth();
+	}
+
+	public void goEast() {
+		detailCanvas.goEast();
+	}
+
+	public void goWest() {
+		detailCanvas.goWest();
+	}
+	
 }

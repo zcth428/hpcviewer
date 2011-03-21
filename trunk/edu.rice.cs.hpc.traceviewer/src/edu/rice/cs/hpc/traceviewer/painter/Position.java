37eeba4ef3c75026820481266f4c4322f6cc5a66
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-03-21 22:06:10 +0000

    make class Position to be serializable so that we can store the info to a file

--- trunk/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/painter/Position.java
+++ trunk/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/painter/Position.java
@@ -1,6 +1,13 @@
 package edu.rice.cs.hpc.traceviewer.painter;
 
-public class Position {
+import java.io.Serializable;
+
+public class Position  implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2287052521974687520L;
+	
 	public long time;
 	public int process;
 	

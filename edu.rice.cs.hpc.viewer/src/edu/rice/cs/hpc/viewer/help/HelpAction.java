bb5a23b5d6ad3181bf1e6f4f9b4ebdfe45c0e2ea
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-05-05 16:47:24 +0000

    minor cosmetics update: code cleanup

--- edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/help/HelpAction.java
+++ edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/help/HelpAction.java
@@ -3,18 +3,12 @@
  */
 package edu.rice.cs.hpc.viewer.help;
 
-import java.io.IOException;
-import java.net.URL;
-
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
-
 import org.eclipse.jface.action.Action;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchPage;
@@ -25,9 +19,9 @@ import org.eclipse.ui.IWorkbenchPage;
  *
  */
 public class HelpAction extends Action {
-	static public String ID="edu.rice.cs.hpc.viewer.help.HelpAction";
+	final static public String ID="edu.rice.cs.hpc.viewer.help.HelpAction";
 	final static private String HELP_FILE_PATH = "/doc/hpcviewer-users-manual.html";
-	protected IWorkbenchWindow objWindow;
+	final private IWorkbenchWindow objWindow;
 	
 	public HelpAction(IWorkbenchWindow window) {
 		this.objWindow = window;
@@ -41,7 +35,6 @@ public class HelpAction extends Action {
 		IWorkspace objWorkspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = objWorkspace.getRoot();
 
-		//String sFilePath = this.getHelpPath();
 		IFile file = root.getFile(new Path(HELP_FILE_PATH));
 		
 		IWorkbenchPage objPage = this.objWindow.getActivePage(); 
@@ -53,32 +46,5 @@ public class HelpAction extends Action {
 			e.printStackTrace();
 		}
 
-	}
-	
-
-	
-	/****
-	 * retrieve help path of the local resource
-	 * @return
-	 */
-	private String getHelpPath() {
-		// URL to the root ("/") of the plugin-path:
-		URL relativeURL = Platform.getBundle("edu.rice.cs.hpc.viewer").getEntry(HELP_FILE_PATH);
-
-		// Turn relative path to a local path with the help of Eclipse-platform:
-		URL localURL;
-		try {
-			localURL = FileLocator.resolve(relativeURL);
-
-			// From this you can get the path
-			String pluginDirString = localURL.getPath();
-			
-			return pluginDirString;
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
 	}
 }

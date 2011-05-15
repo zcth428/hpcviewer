235d8828aa901554aa7f9b82470f6f8a8e34f719
johnmc <johnmc@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-05-15 01:38:42 +0000

    add check to verify that DISPLAY is non-null. normalize the error messages.

--- trunk/edu.rice.cs.hpc.traceviewer/scripts/hpctraceviewer.sh
+++ trunk/edu.rice.cs.hpc.traceviewer/scripts/hpctraceviewer.sh
@@ -23,19 +23,16 @@ fi
 bindir=`dirname "$script"`
 bindir=`( cd "$bindir" && pwd )`
 viewer="${bindir}/../libexec/hpctraceviewer/hpctraceviewer"
-test -x "$viewer" || die "unable to find: $viewer"
+test -x "$viewer"  || die "fatal error - executable $viewer not found"
+test -n "$DISPLAY" || die "fatal error - DISPLAY variable must be set"
 
 #
 # Check java version.
 #
 java_version=`java -version 2>&1 | awk '{print $3}'`
 java_version=${java_version:3:1}
-if test $java_version -ge 5
-then
-  echo "java version: $java_version"
-else
-  echo "Java version is not suppported: $java_version"
-  echo "Please use Java 1.5 (at least)"
+if test $java_version -lt 5
+  echo "$0 fatal error - Java version $java_version is too old; please use at least Java 1.5."
   exit
 fi
 
@@ -45,6 +42,6 @@ fi
 if test -d "$HOME" ; then
     exec "$viewer" -data "$workspace" "$@"
 else
-    echo "$0: warning: HOME is not set, proceeding anyway" 1>&2
+    echo "$0 warning - HOME is not set, proceeding anyway" 1>&2
     exec "$viewer" "$@"
 fi

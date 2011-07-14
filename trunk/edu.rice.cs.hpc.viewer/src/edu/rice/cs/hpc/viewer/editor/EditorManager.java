90dd7a8061d31489b4ac7f0e933011c1273e58f8
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-07-14 22:37:18 +0000

    Separate the splitting staffs into its own class (we will use the split for other editors such as graph)

--- trunk/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/editor/EditorManager.java
+++ trunk/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/editor/EditorManager.java
@@ -7,22 +7,11 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.FileStoreEditorInput;
-import org.eclipse.ui.internal.EditorSashContainer;
-import org.eclipse.ui.internal.EditorStack;
-import org.eclipse.ui.internal.ILayoutContainer;
-import org.eclipse.ui.internal.LayoutPart;
-import org.eclipse.ui.internal.PartPane;
-import org.eclipse.ui.internal.PartSashContainer;
-import org.eclipse.ui.internal.PartSite;
-import org.eclipse.ui.internal.PartStack;
-import org.eclipse.ui.internal.WorkbenchPage;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 import org.eclipse.ui.editors.text.EditorsUI;
 
@@ -36,7 +25,7 @@ import edu.rice.cs.hpc.viewer.util.Utilities;
  * @author la5
  *
  */
-public class EditorManager {
+public class EditorManager extends BaseEditorManager{
     private IWorkbenchWindow windowCurrent;
 
 
@@ -151,39 +140,9 @@ public class EditorManager {
 	 * @throws PartInitException
 	 */
 	private static IEditorPart openEditorOnFileStore(IWorkbenchPage page, IFileStore fileStore, Experiment experiment) throws PartInitException {
-		//sanity checks
-		if (page == null) {
-			throw new IllegalArgumentException();
-		}
 
-		// If there is already a editor area partition for this database, give one of its editors focus 
-		// so that this new editor will display in the same partition.  If not we will create a new partition
-		// and put the new editor in it after we open the editor.
-		IEditorReference[] editorReferences = page.getEditorReferences();
+		boolean needNewPartition = BaseEditorManager.splitBegin(page, experiment);
 		
-		// assume there are no editors open and that we will not need a new partition
-		boolean needNewPartition = false;
-		// if at lease one editor already open, we may need new partition (see below)
-		if (editorReferences.length > 0) {
-			needNewPartition = true;
-		}
-		
-		// check if there is already an editor opened for this database
-		final String expFile = experiment.getXMLExperimentFile().getPath();
-		for(IEditorReference editorRef: editorReferences) {
-			IEditorPart editor = editorRef.getEditor(false);
-			
-			if (editor instanceof IViewerEditor) {
-				Experiment expEditor = ((IViewerEditor)editor).getExperiment();
-				final String expEditorFile = expEditor.getXMLExperimentFile().getPath();
-				if (expFile.equals(expEditorFile)) {
-					// An editor has already opened, do not split the pane
-					needNewPartition = false;
-					break;
-				}
-			}
-		}
-
 		IEditorInput input = getEditorInput(fileStore);
 		//String editorId = getEditorId(fileStore);
 		// forbid eclipse to use an external editor
@@ -198,11 +157,9 @@ public class EditorManager {
 				// database numbers start with 0 but titles start with 1
 				//sce.setPartNamePrefix((dbNum+1) + "-");
 		}
+
+		BaseEditorManager.splitEnd(needNewPartition, iep);
 		
-		// if this is the first editor from this database, create new partition to put it in
-		if (needNewPartition == true) {
-			splitEditorArea(iep);
-		}
 		return iep;
 	}
 
@@ -222,50 +179,5 @@ public class EditorManager {
 	}
 
 	//-------========================= END TAKEN FROM IDE ===============
-
-	
-	/**
-	 * Split the editor area for this new editor.
-	 */
-//	@SuppressWarnings("restriction")
-	private static void splitEditorArea(IEditorPart iep) {
-		PartPane partPane = ((PartSite) iep.getSite()).getPane();
-		// Get PartPane that correspond to the active editor
-		PartPane currentEditorPartPane = ((PartSite) iep.getSite()).getPane();
-		EditorSashContainer editorSashContainer = null;
-		ILayoutContainer rootLayoutContainer = partPane.getPart().getContainer();
-		if (rootLayoutContainer instanceof LayoutPart) {
-			ILayoutContainer editorSashLayoutContainer = ((LayoutPart) rootLayoutContainer).getContainer();
-			if (editorSashLayoutContainer instanceof EditorSashContainer) {
-				editorSashContainer = ((EditorSashContainer) editorSashLayoutContainer);
-			}
-		}
-
-		/*
-		 * Create a new part stack (i.e. a workbook) to home the
-		 * currentEditorPartPane which hold the active editor
-		 */
-		PartStack newPart = createStack(editorSashContainer);
-		editorSashContainer.stack(currentEditorPartPane, newPart);
-		if (rootLayoutContainer instanceof LayoutPart) {
-			ILayoutContainer cont = ((LayoutPart) rootLayoutContainer).getContainer();
-			if (cont instanceof PartSashContainer) {
-				// "Split" the editor area by adding the new part
-				((PartSashContainer) cont).add(newPart);
-			}
-		}
-	}
-	/**
-	 * A method to create a part stack container (a new workbook)
-	 * 
-	 * @param editorSashContainer the <code>EditorSashContainer</code> to set for the returned <code>PartStack</code>
-	 * @return a new part stack container
-	 */
-//	@SuppressWarnings("restriction")
-	private static PartStack createStack(EditorSashContainer editorSashContainer) {
-		IWorkbenchPage workbenchPage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		EditorStack newWorkbook = EditorStack.newEditorWorkbook(editorSashContainer, (WorkbenchPage)workbenchPage);
-		return newWorkbook;
-	}
 
 }

e3138670ee169503fd7f2fcfaeb9ff552c28ab83
mab093020 <mab093020@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-07-22 21:21:21 +0000

    Initial Protocol buffers Java

---
+++ branches/pb/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/ui/ITraceViewAction.java
@@ -0,0 +1,24 @@
+package edu.rice.cs.hpc.traceviewer.ui;
+
+public interface ITraceViewAction {
+
+	public void home();
+
+	public void timeZoomIn();
+	public void timeZoomOut();
+	
+	public void processZoomIn();
+	public void processZoomOut();
+
+	public void undo();
+	public void redo();
+	
+	public void save();
+	public void open();
+	
+	public void goNorth();
+	public void goSouth();
+	public void goEast();
+	public void goWest();
+	
+}

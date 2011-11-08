e0e7c514a0427b447fdf588355370a5f691841c8
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2011-11-08 18:07:20 +0000

    Tag for official release of hpcviewer 5.2.0 Note: This release includes also hpcdata and hpctraceviewer

---
+++ tags/hpcviewer-5.2.0-release/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/actions/OpenDatabase.java
@@ -0,0 +1,25 @@
+package edu.rice.cs.hpc.traceviewer.actions;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import edu.rice.cs.hpc.traceviewer.db.TraceDatabase;
+
+public class OpenDatabase extends AbstractHandler
+{
+	
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		final IWorkbenchPartSite site = window.getActivePage().getActivePart().getSite();
+		final IViewSite vSite = ( IViewSite ) site;
+
+		TraceDatabase.openDatabase(window, null, vSite.getActionBars().getStatusLineManager());
+		return null;
+	}
+}

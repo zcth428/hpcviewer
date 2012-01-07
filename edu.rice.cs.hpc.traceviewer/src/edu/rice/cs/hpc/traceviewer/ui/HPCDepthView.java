f1db8435f0b714857598a08bb11ba775084b8122
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-01-07 00:49:41 +0000

    Release for 5.2.1: - bug fixes for graph on fused multiple experiments - bug fixes for hpctraceviewer when data is fit into the display (not to use buggy binary fill) - performance enhancement on hpctraceviewer

---
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/ui/HPCDepthView.java
@@ -0,0 +1,91 @@
+package edu.rice.cs.hpc.traceviewer.ui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+import edu.rice.cs.hpc.traceviewer.events.ITraceDepth;
+import edu.rice.cs.hpc.traceviewer.events.ITracePosition;
+import edu.rice.cs.hpc.traceviewer.painter.DepthTimeCanvas;
+import edu.rice.cs.hpc.traceviewer.painter.Position;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
+
+public class HPCDepthView extends ViewPart implements ITraceDepth, ITracePosition
+{
+	public static final String ID = "hpcdepthview.view";
+	
+	/**The composite that holds everything in the view*/
+	Composite master;
+	
+	/**The max depth of the space time data.*/
+	int maxDepth;
+	
+	/** Paints and displays the detail view. */
+	DepthTimeCanvas depthCanvas;
+	
+	HPCTraceView traceview;
+	
+	public void createPartControl(Composite _master)
+	{
+		master = _master;
+		
+		try 
+		{
+			traceview = (HPCTraceView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(HPCTraceView.ID);
+		}
+		catch (PartInitException e) 
+		{
+			traceview = null;
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		setupEverything();
+	}
+	
+	public void setupEverything()
+	{
+		/*************************************************************************
+		 * Master Composite
+		 */
+		
+		master.setLayout(new GridLayout());
+		master.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		/*************************************************************************
+		 * Depth View Canvas
+		 */
+		
+		depthCanvas = new DepthTimeCanvas(master, traceview.detailCanvas, 0);
+		depthCanvas.setLayout(new GridLayout());
+		depthCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		depthCanvas.setVisible(false);
+		
+		traceview.detailCanvas.setDepthCanvas(depthCanvas);
+	}
+	
+	public void updateData(SpaceTimeData _stData)
+	{
+		this.depthCanvas.updateData(_stData);
+		_stData.addDepthListener(this);
+		_stData.addPositionListener(this);
+		depthCanvas.setVisible(true);
+	}
+
+	public void setFocus()
+	{
+		this.depthCanvas.setFocus();
+	}
+
+	public void setDepth(int new_depth) {
+		this.depthCanvas.setDepth(new_depth);
+	}
+
+	public void setPosition(Position position) {
+		this.depthCanvas.setPosition(position);
+	}
+}

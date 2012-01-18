d3da5889b509f1f8777f5a7dd8682fe9ed6853fd
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-01-18 19:01:50 +0000

    Bug fix: handling different active sites to retrieve status bar.
    In Eclipse, a status bar is hosted by either a view or an editor. Unfortunately, in some cases, the java operator "instanceof" fail to detect if an object is an instantiation of an interface or normal class. Not sure how this can occur. The only easy hack solution is to provide every possible instantiation of a site, and then retrieve its status bar.

--- trunk/edu.rice.cs.hpc.common/src/edu/rice/cs/hpc/common/ui/Util.java
+++ trunk/edu.rice.cs.hpc.common/src/edu/rice/cs/hpc/common/ui/Util.java
@@ -8,6 +8,8 @@ import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.EditorSite;
+import org.eclipse.ui.part.EditorPart;
 
 public class Util {
 	
@@ -22,11 +24,21 @@ public class Util {
 		// --------------------------------------------------------------
 		// the current active site can be either editor or view
 		// if none of them is active, then we have nothing
+		//
+		//	@TODO ugly code: this is a hack version to identify the current "site"
+		//		   status bar is hosted in a view site or editor site. Other than that, we are doomed.
 		// --------------------------------------------------------------
 		if (site instanceof IViewSite)
 			statusLine = ((IViewSite)site).getActionBars().getStatusLineManager();
 		else if (site instanceof IEditorPart)
 			statusLine = ((IEditorSite)site).getActionBars().getStatusLineManager();
+		else if (site instanceof EditorSite)
+			statusLine = ((EditorSite)site).getActionBars().getStatusLineManager();
+		else if (site instanceof EditorPart){
+			statusLine = ((EditorPart) site).getEditorSite().getActionBars().getStatusLineManager();
+		} else {
+			System.out.println("unknown active site: " + site + " \t class: " + site.getClass());
+		}
 
 		return statusLine;
 	}

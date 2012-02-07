384e43cb302c08d03214b7ed7489eb8969156bc9
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-02-07 21:54:19 +0000

    creating a new branch for amd project

---
+++ branches/amd/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/metric/FinalMetric.java
@@ -0,0 +1,24 @@
+package edu.rice.cs.hpc.data.experiment.metric;
+
+
+public class FinalMetric extends Metric {
+
+	public FinalMetric(String shortName,
+			String nativeName, String displayName, boolean displayed, String format,
+			AnnotationType annotationType, String sampleperiod, MetricType metricType,
+			int partnerIndex) {
+		super(shortName, nativeName, displayName, displayed, format,
+				annotationType, sampleperiod, metricType, partnerIndex);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FinalMetric(String shortName,
+			String nativeName, String displayName, boolean displayed, String format,
+			AnnotationType annotationType, double sampleperiod, MetricType metricType,
+			int partnerIndex) {
+		super(shortName, nativeName, displayName, displayed, format,
+				annotationType, sampleperiod, metricType, partnerIndex);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ branches/amd/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/metric/Metric.java
@@ -0,0 +1,143 @@
+//////////////////////////////////////////////////////////////////////////
+//																		//
+//	Metric.java															//
+//																		//
+//	experiment.Metric -- a metric and its data in an experiment			//
+//	Last edited: January 15, 2002 at 12:37 am							//
+//																		//
+//	(c) Copyright 2002 Rice University. All rights reserved.			//
+//																		//
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+package edu.rice.cs.hpc.data.experiment.metric;
+
+
+import edu.rice.cs.hpc.data.experiment.metric.MetricType;
+import edu.rice.cs.hpc.data.experiment.scope.Scope;
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//	CLASS METRIC														//
+//////////////////////////////////////////////////////////////////////////
+
+ /**
+ *
+ * A metric and its data in an HPCView experiment.
+ *
+ */
+
+
+public class Metric extends BaseMetric
+{
+
+protected int partnerIndex;
+
+public final static int NO_PARTNER_INDEX = -1;
+
+
+//////////////////////////////////////////////////////////////////////////
+//	INITIALIZATION														//
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+/*************************************************************************
+ *	Creates a Metric.
+ ************************************************************************/
+	
+/**
+ * Construct a metric using a "String" sample period
+ * @param shortName
+ * @param nativeName
+ * @param displayName
+ * @param displayed
+ * @param format
+ * @param annotationType
+ * @param sampleperiod
+ * @param metricType
+ * @param partnerIndex
+ */
+public Metric(String shortName, String nativeName, String displayName, boolean displayed, 
+              String format, AnnotationType annotationType, String samplePeriod, MetricType metricType, int partnerIndex)
+{
+	super(shortName, displayName, displayed, format, annotationType, 0, metricType);
+	// creation arguments
+	this.nativeName  = nativeName;
+    this.sampleperiod  = this.convertSamplePeriode(samplePeriod);
+    this.metricType     = metricType;
+    this.partnerIndex = partnerIndex;
+}
+
+
+/**
+ * Construct a metric using a "double" sample period
+ * @param shortName
+ * @param nativeName
+ * @param displayName
+ * @param displayed
+ * @param format
+ * @param annotationType
+ * @param sampleperiod
+ * @param metricType
+ * @param partnerIndex
+ */
+public Metric( String shortName, String nativeName, String displayName, boolean displayed, 
+               String format, AnnotationType annotationType, double samplePeriod, MetricType metricType, int partnerIndex)
+{
+	super(shortName, displayName, displayed, format, annotationType, 0, metricType);
+	// creation arguments
+	this.nativeName  = nativeName;
+	this.sampleperiod  = samplePeriod;
+	this.metricType     = metricType;
+	this.partnerIndex = partnerIndex;
+}
+
+/*************************************************************************
+ *	Returns the value of this metric at a given scope.
+ ************************************************************************/	
+public MetricValue getValue(Scope s)
+{
+	return s.getMetricValue(this);
+}
+
+/*************************************************************************
+ *	Returns the minimum value this metric can take.
+ ************************************************************************/	
+public MetricValue getMinimumValue()
+{
+	return new MetricValue(0.0);
+}
+
+/*************************************************************************
+ *	Returns the maximum value this metric can take.
+ ************************************************************************/
+	
+public MetricValue getMaximumValue()
+{
+	return new MetricValue(Double.MAX_VALUE);
+}
+
+public int getPartnerIndex()
+{
+	return this.partnerIndex;
+}
+
+public void setPartnerIndex(int ei)
+{
+	this.partnerIndex = ei;
+}
+
+}
+
+
+
+
+
+
+
+

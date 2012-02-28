e27b7137de1dfc5737c03908122e4844bf42f87b
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-02-28 21:54:35 +0000

    draft merging database

--- branches/amd/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/MergeScopeTreesVisitor.java
+++ branches/amd/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/MergeScopeTreesVisitor.java
@@ -19,18 +19,21 @@ import edu.rice.cs.hpc.data.experiment.scope.filters.MetricValuePropagationFilte
 public class MergeScopeTreesVisitor implements IScopeVisitor {
 	final private Stack<Scope> scopeStack;
 	final private MetricValuePropagationFilter filter;
+	final private int offsetMetric;
 
-	public MergeScopeTreesVisitor(Scope newRoot, MetricValuePropagationFilter filter) {
+	public MergeScopeTreesVisitor(Scope newRoot, MetricValuePropagationFilter filter, int offset) {
 		scopeStack = new Stack<Scope>();
 		scopeStack.push(newRoot);
 		this.filter = filter;
+		offsetMetric = offset;
 	}
 	
 	//----------------------------------------------------
 	// visitor pattern instantiations for each Scope type
 	//----------------------------------------------------
 	public void visit(RootScope scope, 				ScopeVisitType vt) { 
-		if (scope.getType() != RootScopeType.Invisible)	mergeInsert(scope, vt);
+		if (scope.getType() != RootScopeType.Invisible)	
+			mergeInsert(scope, vt);
 	}
 	public void visit(LoadModuleScope scope, 		ScopeVisitType vt) { mergeInsert(scope, vt); }
 	public void visit(FileScope scope, 				ScopeVisitType vt) { mergeInsert(scope, vt); }
@@ -56,16 +59,60 @@ public class MergeScopeTreesVisitor implements IScopeVisitor {
 		}
 	}
 	
+	/***
+	 * find if any of the child matches
+	 * 
+	 * @param parent
+	 * @param toMatch
+	 * @return
+	 */
 	private Scope findMatch(Scope parent, Scope toMatch) {
 		for (int i=0; i< parent.getSubscopeCount(); i++) {
 			Scope kid = parent.getSubscope(i);
 
-			if (kid.getName().equals(toMatch.getName()))
+			if (isTheSame(kid, toMatch))
 				return kid;
 		}
 		return null;
 	}
 	
+	
+	/****
+	 * check if two scopes are "hierarchically" the same
+	 * 
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
+	private boolean isTheSame(Scope s1, Scope s2)
+	{
+		boolean ret = false;
+		
+		if (s1 instanceof RootScope && s2 instanceof RootScope) 
+		{	// skip the root scope
+			ret = true;
+		} else if (s1.getName().equals(s2.getName()))
+		{
+			// exactly the same name, check if hierarchically the same
+			final Scope p1 = s1.getParentScope();
+			final Scope p2 = s2.getParentScope();
+			
+			if (p1.getChildCount() == p2.getChildCount()) {
+				ret = true;
+			}
+		}
+		return ret;
+		
+	}
+	
+	/*****
+	 * add child and its metric values if needed
+	 * 
+	 * @param parent
+	 * @param target
+	 * @param source
+	 * @return
+	 */
 	private Scope addMetricColumns(Scope parent, Scope target, Scope source) {
 		
 		if (target == null) {
@@ -96,10 +143,9 @@ public class MergeScopeTreesVisitor implements IScopeVisitor {
 		
 		final Experiment experiment = (Experiment)source.getExperiment();
 		final int numSourceMetrics = experiment.getMetricCount();
-		final int numTargetMetrics = ((Experiment)target.getExperiment()).getMetricCount();
 		
 		for (int i = 0; i< numSourceMetrics; i++) {
-			target.accumulateMetric(source, i, numTargetMetrics+i, filter);
+			target.accumulateMetric(source, i, offsetMetric+i, filter);
 		}
 	}
 	

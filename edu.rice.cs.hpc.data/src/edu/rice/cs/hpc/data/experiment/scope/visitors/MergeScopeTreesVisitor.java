da7a61daf46a9bb19d59df89e86f7522978e74d4
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-03-01 06:06:33 +0000

    refine merging for loops. loops are tricky since they are major source of optimization, hence the same loop from different executable can be completely different. However, if the children of the loops do mostly the same thing, and the position in the code is not too different, we may consider as matched.

--- edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/MergeScopeTreesVisitor.java
+++ edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/MergeScopeTreesVisitor.java
@@ -10,92 +10,30 @@
 //////////////////////////////////////////////////////////////////////////
 package edu.rice.cs.hpc.data.experiment.scope.visitors;
 
+import edu.rice.cs.hpc.data.experiment.merge.MatchScope;
 import edu.rice.cs.hpc.data.experiment.scope.*;
 
 public class MergeScopeTreesVisitor extends BaseDuplicateScopeTreesVisitor {
-
+	
+	final private MatchScope matcher; 
+	
 	public MergeScopeTreesVisitor(Scope newRoot, int offset) {
 		super(newRoot, offset);
+		matcher = new MatchScope();
 	}
 	
 	
 	@Override
 	protected Scope findMatch(Scope parent, Scope toMatch) {
+		
 		for (int i=0; i< parent.getSubscopeCount(); i++) {
 			Scope kid = parent.getSubscope(i);
 
-			if (kid.isCounterZero() && isTheSame(kid, toMatch)) {
+			if (kid.isCounterZero() && matcher.isMatch(kid, toMatch)) {
 				kid.incrementCounter();
 				return kid;
 			}
 		}
 		return null;
-	}
-	
-	
-	/****
-	 * check if two scopes are "hierarchically" the same
-	 * 
-	 * @param s1
-	 * @param s2
-	 * @return
-	 */
-	private boolean isTheSame(Scope s1, Scope s2)
-	{
-		boolean ret = false;
-		String s = "diff";
-		
-		if (s1 instanceof RootScope && s2 instanceof RootScope) 
-		{	// skip the root scope
-			ret = true;
-		} else if (s1.hashCode() == s2.hashCode())
-		{
-			// exactly the same flat id, check if hierarchically the same
-			int d1 = s1.getCCTIndex();
-			int d2 = s2.getCCTIndex();
-
-			// the same cct ?
-			if (d1 == d2) {
-				// ideal case: the same cct
-				ret = true;
-				s = "scct";
-			} else {
-				// the same flat index, but different cct index
-				// this may be the case of recursive function of fib(a) + fib(b)
-				if (hasTheSameNumberOfSiblings(s1, s2)) {
-					// has the same number of siblings
-					ret = true;
-					s = "ssi";
-				} 			
-			}
-			System.out.println("MSTV ["+this.scopeStack.size()+"] " + s1 + ", c1: " + s1.getCCTIndex() + 
-					", c2: " + s2.getCCTIndex() + "\ts: " + s);
-			
-		} else if (s1.getName().equals(s2.getName())) 
-		{
-			// different hash index, but the same name
-			// this can be because of optimization
-			
-			if (hasTheSameNumberOfSiblings(s1, s2)) {
-				// has the same number of siblings: it's likely to be the same code
-				ret = true;
-				s = "ssi";
-			} 			
-			System.out.println("MSTV ["+this.scopeStack.size()+"] " + s1 + ", c1: " + s1.getCCTIndex() + 
-					", c2: " + s2.getCCTIndex() + "\ts: " + s);
-		}
-		return ret;
 	}	
-	
-	
-	private boolean hasTheSameNumberOfSiblings(Scope s1, Scope s2) 
-	{
-		// exactly the same name, but perhaps different line number
-		final Scope p1 = s1.getParentScope();
-		final Scope p2 = s2.getParentScope();
-		int d1 = p1.getChildCount();
-		int d2 = p2.getChildCount();
-		
-		return (d1==d2);
-	}
 }

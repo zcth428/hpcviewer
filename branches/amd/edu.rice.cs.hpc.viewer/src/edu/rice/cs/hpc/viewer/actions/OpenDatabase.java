af94ac3a452e95ba42556dcac22ad9bcae549385
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-03-13 17:27:44 +0000

    Fix enable/disable menus for "close" and "merge" databases by using declarative variables in plugin.xml

--- branches/amd/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/actions/OpenDatabase.java
+++ branches/amd/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/actions/OpenDatabase.java
@@ -5,29 +5,17 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.services.ISourceProviderService;
-
 import edu.rice.cs.hpc.viewer.experiment.ExperimentManager;
-import edu.rice.cs.hpc.viewer.provider.DatabaseState;
 
 public class OpenDatabase extends AbstractHandler {
 
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	public Object execute(ExecutionEvent event) throws ExecutionException 
+	{
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
 		final ExperimentManager expFile = new ExperimentManager(window); 
 				//ExperimentData.getInstance(window).getExperimentManager();
 		expFile.openFileExperiment(ExperimentManager.FLAG_DEFAULT);
-
-		ISourceProviderService sourceProviderService = (ISourceProviderService) HandlerUtil
-				.getActiveWorkbenchWindow(event).getService(
-						ISourceProviderService.class);
-		// Now get my service
-		DatabaseState commandStateService = (DatabaseState) sourceProviderService
-				.getSourceProvider(DatabaseState.MY_STATE);
-		commandStateService.toogleEnabled();
-		System.out.println("Change to " + commandStateService.getToogle());
 
 		
 		return null;
--- branches/amd/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/provider/DatabaseState.java
+++ branches/amd/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/provider/DatabaseState.java
@@ -5,41 +5,50 @@ import java.util.Map;
 
 import org.eclipse.ui.AbstractSourceProvider;
 import org.eclipse.ui.ISources;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import edu.rice.cs.hpc.viewer.window.ViewerWindow;
+import edu.rice.cs.hpc.viewer.window.ViewerWindowManager;
 
 public class DatabaseState extends AbstractSourceProvider {
 
-	public final static String MY_STATE = "edu.rice.cs.hpc.viewer.provider.data.active";
+	public final static String DATABASE_ACTIVE_STATE = "edu.rice.cs.hpc.viewer.provider.data.active";
+	public final static String DATABASE_MERGE_STATE = "edu.rice.cs.hpc.viewer.provider.data.merge";
 	public final static String ENABLED = "ENABLED";
-	public final static String DISENABLED = "DISABLED";
-	private boolean enabled = false;
-
+	public final static String DISABLED = "DISABLED";
+	
+	private int num_opened_database = 0;
 	
 	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
-	public Map getCurrentState() {
+	public Map getCurrentState() 
+	{
 		Map<String, Object> map = new HashMap<String, Object>(1);
-		String value = enabled ? ENABLED : DISENABLED;
-		map.put(MY_STATE, value);
+		String value = num_opened_database>0 ? ENABLED : DISABLED;
+		map.put(DATABASE_ACTIVE_STATE, value);
+		
+		value = num_opened_database>1 ? ENABLED : DISABLED;
+		map.put(DATABASE_MERGE_STATE, value);
+		
 		return map;
 	}
 
-	public String[] getProvidedSourceNames() {
-		return new String[] { MY_STATE };
-	}
-
-
-	public void toogleEnabled() {
-		enabled = !enabled ;
-		String value = enabled ? ENABLED : DISENABLED;
-		fireSourceChanged(ISources.WORKBENCH, MY_STATE, value);
-	}
-	
-	public boolean getToogle()
+	public String[] getProvidedSourceNames() 
 	{
-		return enabled;
+		return new String[] { DATABASE_ACTIVE_STATE, DATABASE_MERGE_STATE };
 	}
 
+
+	public void toogleEnabled(IWorkbenchWindow window) 
+	{
+		ViewerWindow vw = ViewerWindowManager.getViewerWindow(window);
+		
+		num_opened_database = vw.getOpenDatabases();
+		String value = num_opened_database>0 ? ENABLED : DISABLED;
+		fireSourceChanged(ISources.WORKBENCH, DATABASE_ACTIVE_STATE, value);
+		
+		value = num_opened_database>1 ? ENABLED : DISABLED;
+		fireSourceChanged(ISources.WORKBENCH, DATABASE_MERGE_STATE, value);
+	}	
 }

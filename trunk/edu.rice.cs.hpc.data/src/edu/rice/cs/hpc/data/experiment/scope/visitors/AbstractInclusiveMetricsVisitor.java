38bb32dedeb7707067dc05b82ad530fd2c14bd4d
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-04-12 18:58:46 +0000

    Clean-up the code before fixing bug in hpcdata, Bug reported by Nathan: there's inconsistency of metric values in callers tree. In fact, the problem is not in callers tree, but from the computation of inclusive metrics. Somehow, metric in the leaf scope is not summed up to the parent. Before looking more details, we needs to clean-up the code first. There are some dead codes, thus it makes hard to read.

--- trunk/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/AbstractInclusiveMetricsVisitor.java
+++ trunk/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/visitors/AbstractInclusiveMetricsVisitor.java
@@ -13,21 +13,15 @@ import edu.rice.cs.hpc.data.experiment.scope.RootScope;
 import edu.rice.cs.hpc.data.experiment.scope.Scope;
 import edu.rice.cs.hpc.data.experiment.scope.ScopeVisitType;
 import edu.rice.cs.hpc.data.experiment.scope.StatementRangeScope;
-import edu.rice.cs.hpc.data.experiment.scope.filters.ExclusiveOnlyMetricPropagationFilter;
-import edu.rice.cs.hpc.data.experiment.scope.filters.FlatViewInclMetricPropagationFilter;
 import edu.rice.cs.hpc.data.experiment.scope.filters.InclusiveOnlyMetricPropagationFilter;
 import edu.rice.cs.hpc.data.experiment.scope.filters.MetricValuePropagationFilter;
 
 public abstract class AbstractInclusiveMetricsVisitor implements IScopeVisitor {
 
 	protected MetricValuePropagationFilter filter;
-	protected ExclusiveOnlyMetricPropagationFilter filterExclusive ;
-	private InclusiveOnlyMetricPropagationFilter filterInclusive;
 
 	public AbstractInclusiveMetricsVisitor(Experiment experiment, MetricValuePropagationFilter currentFilter) {
 		this.filter = currentFilter;
-		filterExclusive = new ExclusiveOnlyMetricPropagationFilter(experiment);
-		filterInclusive = new InclusiveOnlyMetricPropagationFilter(experiment);
 	}
 	//----------------------------------------------------
 	// visitor pattern instantiations for each Scope type
@@ -54,24 +48,8 @@ public abstract class AbstractInclusiveMetricsVisitor implements IScopeVisitor {
 			Scope parent = scope.getParentScope();
 			if (parent != null) {
 				if (scope instanceof CallSiteScope) {
-					// in case of FLAT VIEW, we need to specially deal with recursive functions:
-					//	avoid recomputation of the cost
-					if (filter instanceof FlatViewInclMetricPropagationFilter){
-						// Exclusive metrics: Add the cost of the line scope into the parent
-						accumulateToParent( parent, ((CallSiteScope)scope).getLineScope(), this.filterExclusive );
-						//parent.accumulateMetrics(((CallSiteScope)scope).getLineScope(), 
-						//		this.filterExclusive, numberOfPrimaryMetrics);
-						// Inclusive metrics: if the parent is not procedure, add the scope into the parent
-						// the cost of the procedure scope should have to be precomputed in the FlatViewScopeVisitor class
-						if(!(parent instanceof ProcedureScope))
-							accumulateToParent( parent, scope, this.filterInclusive );
-							//parent.accumulateMetrics(scope, 
-							//	this.filterInclusive, numberOfPrimaryMetrics);
-					} else {
-						// inclusive view: add everything
-						accumulateToParent( parent, scope );
-						//parent.accumulateMetrics(scope, filter, numberOfPrimaryMetrics);						
-					}
+					// inclusive view: add everything
+					accumulateToParent( parent, scope );
 				} else {
 					// New definition of exclusive cost:
 					//	The cost of Outer loop does not include the cost of inner loop 
@@ -81,17 +59,10 @@ public abstract class AbstractInclusiveMetricsVisitor implements IScopeVisitor {
 							// During the creation of CCT, we call this class twice: one for exclusive, the other for incl
 							// so we need to make sure that only the inclusive is taken
 							accumulateToParent( parent, scope );
-							//parent.accumulateMetrics(scope, this.filter, this.numberOfPrimaryMetrics);
-						} else if (filter instanceof FlatViewInclMetricPropagationFilter) {
-							// This path is from flat tree construction, we just take into account inclusive loops
-							accumulateToParent( parent, scope, this.filterInclusive );
-							//parent.accumulateMetrics(scope, this.filterInclusive, this.numberOfPrimaryMetrics);
-							this.accumulateAncestor(scope, scope);
 						}
 						return;
 					} 
 					accumulateToParent( parent, scope );
-					//parent.accumulateMetrics(scope, filter, numberOfPrimaryMetrics);
 				}
 			}
 		}
@@ -104,36 +75,4 @@ public abstract class AbstractInclusiveMetricsVisitor implements IScopeVisitor {
 	 * @param source
 	 */
 	abstract protected void accumulateToParent(Scope parent, Scope source) ;
-	
-	/**
-	 * Method to accumulate the metric value from the child to the parent based on a given filter
-	 * @param parent
-	 * @param source
-	 * @param filter
-	 */
-	abstract protected void accumulateToParent(Scope parent, Scope source, MetricValuePropagationFilter myfilter) ;
-
-	
-	/**
-	 * Accumulate the exclusive cost of a scope into its procedure scope.
-	 * Specifically designed for Flat View.
-	 * @param scope
-	 * @param parent
-	 */
-	protected void accumulateAncestor(Scope scope, Scope parent) {
-		Scope ancestor = parent.getParentScope();
-		while((ancestor != null ) && !(ancestor instanceof RootScope) &&
-				!(ancestor instanceof ProcedureScope) && !(ancestor instanceof CallSiteScope)) {
-			ancestor = ancestor.getParentScope();
-		}
-		if(ancestor != null && (ancestor instanceof ProcedureScope) ) {
-			ProcedureScope scopeAncestorProc = (ProcedureScope) ancestor;
-			if (!scopeAncestorProc.isAlien())
-				accumulateToParent( ancestor, scope, this.filterExclusive );
-				//ancestor.accumulateMetrics(scope, this.filterExclusive, this.numberOfPrimaryMetrics);
-			//else
-			//	System.err.println("IMSV alien detected for "+scope.getName()+"\t: "+scopeAncestorProc.getName());
-		}
-	}
-
 }

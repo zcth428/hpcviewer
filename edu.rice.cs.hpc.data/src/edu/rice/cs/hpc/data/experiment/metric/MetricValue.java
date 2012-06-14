341f440ded0097d24802bde509d2e204dd3febaf
tallent <tallent@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-06-14 18:06:25 +0000

    Correct indentation.

--- edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/metric/MetricValue.java
+++ edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/metric/MetricValue.java
@@ -24,7 +24,7 @@ import edu.rice.cs.hpc.data.util.*;
 //	CLASS METRIC-VALUE													//
 //////////////////////////////////////////////////////////////////////////
 
- /**
+/**
  *
  * A value of a metric at some scope.
  *
@@ -36,12 +36,12 @@ public final class MetricValue
 
 	/** The actual value if available. */
 	protected float value;
-	
+
 	/** The annotation value if available. */
 	protected float annotation;
 
 	protected byte flags;
-	
+
 	protected static final byte VALUE_IS_AVAILABLE = 1;
 	protected static final byte ANNOTATION_IS_AVAILABLE = 2;
 
@@ -51,216 +51,216 @@ public final class MetricValue
 
 
 
-//////////////////////////////////////////////////////////////////////////
-//	INITIALIZATION														//
-//////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	//	INITIALIZATION														//
+	//////////////////////////////////////////////////////////////////////////
 
 
 
 
-/*************************************************************************
- *	Creates an unavailable MetricValue.
- ************************************************************************/
-	
-public MetricValue()
-{
-	setAvailable(this, false);
-	setAnnotationAvailable(this, false);
-}
+	/*************************************************************************
+	 *	Creates an unavailable MetricValue.
+	 ************************************************************************/
 
-
-
-
-/*************************************************************************
- *	Creates an available MetricValue with a given value and annotation value.
- ************************************************************************/
-	
-public MetricValue(double value, double annotation)
-{
-	setValue(this, value);
-	setAnnotationValue(this, ((float)annotation));
-}
-
-
-
-
-/*************************************************************************
- *	Creates an available MetricValue with a given value.
- ************************************************************************/
-	
-public MetricValue(double value)
-{
-	setValue(this, value);
-	setAvailable(this, true);
-	setAnnotationAvailable(this, false);
-}
-
-
-
-//////////////////////////////////////////////////////////////////////////
-//	ACCESS TO VALUE														//
-//////////////////////////////////////////////////////////////////////////
-
-/*************************************************************************
- *	Returns whether the actual value is available.
- ************************************************************************/
-	
-private static boolean getAvailable(MetricValue m)
-{
-	boolean available = (m.flags & VALUE_IS_AVAILABLE) == VALUE_IS_AVAILABLE;
-	return available;
-}
-
-private static void setAvailable(MetricValue m, boolean status)
-{
-	if (status) {
-		m.flags |= VALUE_IS_AVAILABLE;
-	} else {
-		m.flags &= ~VALUE_IS_AVAILABLE;
-	}	
-}
-
-
-private static boolean getAnnotationAvailable(MetricValue m)
-{
-	boolean available = (m.flags & ANNOTATION_IS_AVAILABLE) == ANNOTATION_IS_AVAILABLE;
-	return available;
-}
-
-public static void setAnnotationAvailable(MetricValue m, boolean status)
-{
-	if (status) {
-		m.flags |= ANNOTATION_IS_AVAILABLE;
-	} else {
-		m.flags &= ~ANNOTATION_IS_AVAILABLE;
-	}	
-}
-
-
-public float getValue()
-{
-	boolean available = (flags & VALUE_IS_AVAILABLE) == VALUE_IS_AVAILABLE;
-	Dialogs.Assert(available, "MetricValue::getValue");
-	return this.value;
-}
-
-/*************************************************************************
- *	Returns whether the metric value is available.
- ************************************************************************/
-	
-public static boolean isAvailable(MetricValue m)
-{
-	return ( (m != MetricValue.NONE) && getAvailable(m) && (m.value != 0.0) );
-}
-
-
-
-
-/*************************************************************************
- *	Returns the actual value if available.
- ************************************************************************/
-	
-public static float getValue(MetricValue m)
-{
-	return m.getValue();
-}
-
-
-
-
-/*************************************************************************
- *	Makes the given actual value available.
- ************************************************************************/
-	
-public static void setValue(MetricValue m, double value)
-{
-	setAvailable(m, true);
-	m.value = (float) value;
-}
-
-
-
-
-/*************************************************************************
- *	Returns whether the annotation value is available.
- ************************************************************************/
-	
-public static boolean isAnnotationAvailable(MetricValue m)
-{
-	return (m != MetricValue.NONE) && getAnnotationAvailable(m);
-}
-
-
-
-
-/*************************************************************************
- *	Returns the annotation value if available.
- ************************************************************************/
-	
-public static float getAnnotationValue(MetricValue m)
-{
-	return m.annotation;
-}
-
-
-
-
-/*************************************************************************
- *	Makes the given annotation value available.
- ************************************************************************/
-
-public static void setAnnotationValue(MetricValue m, double annotation)
-{
-	setAnnotationAvailable(m, true);
-	m.annotation = (float) annotation;
-}
-
-
-public static void setAnnotationValue(MetricValue m, float annotation)
-{
-	setAnnotationAvailable(m, true);
-	m.annotation = annotation;
-}
-
-
-public static boolean isZero(MetricValue m) 
-{
-	if (m != MetricValue.NONE) {
-		return ( Double.compare(0.0, m.value) == 0 );
-	}
-	return true;
-}
-
-/*************************************************************************
- *	Compares the metric value to another one.
- *
- *	Unavailable or nonexistent values are treated as less than available values.
- *	Annotation values are ignored.
- *
- ************************************************************************/
-	
-public static int compareTo(MetricValue left, MetricValue right)
-{
-	int result;
-	
-	if( MetricValue.isAvailable(left) && MetricValue.isAvailable(right) )
+	public MetricValue()
 	{
-		if( left.value > right.value )
+		setAvailable(this, false);
+		setAnnotationAvailable(this, false);
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Creates an available MetricValue with a given value and annotation value.
+	 ************************************************************************/
+
+	public MetricValue(double value, double annotation)
+	{
+		setValue(this, value);
+		setAnnotationValue(this, ((float)annotation));
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Creates an available MetricValue with a given value.
+	 ************************************************************************/
+
+	public MetricValue(double value)
+	{
+		setValue(this, value);
+		setAvailable(this, true);
+		setAnnotationAvailable(this, false);
+	}
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+	//	ACCESS TO VALUE														//
+	//////////////////////////////////////////////////////////////////////////
+
+	/*************************************************************************
+	 *	Returns whether the actual value is available.
+	 ************************************************************************/
+
+	private static boolean getAvailable(MetricValue m)
+	{
+		boolean available = (m.flags & VALUE_IS_AVAILABLE) == VALUE_IS_AVAILABLE;
+		return available;
+	}
+
+	private static void setAvailable(MetricValue m, boolean status)
+	{
+		if (status) {
+			m.flags |= VALUE_IS_AVAILABLE;
+		} else {
+			m.flags &= ~VALUE_IS_AVAILABLE;
+		}	
+	}
+
+
+	private static boolean getAnnotationAvailable(MetricValue m)
+	{
+		boolean available = (m.flags & ANNOTATION_IS_AVAILABLE) == ANNOTATION_IS_AVAILABLE;
+		return available;
+	}
+
+	public static void setAnnotationAvailable(MetricValue m, boolean status)
+	{
+		if (status) {
+			m.flags |= ANNOTATION_IS_AVAILABLE;
+		} else {
+			m.flags &= ~ANNOTATION_IS_AVAILABLE;
+		}	
+	}
+
+
+	public float getValue()
+	{
+		boolean available = (flags & VALUE_IS_AVAILABLE) == VALUE_IS_AVAILABLE;
+		Dialogs.Assert(available, "MetricValue::getValue");
+		return this.value;
+	}
+
+	/*************************************************************************
+	 *	Returns whether the metric value is available.
+	 ************************************************************************/
+
+	public static boolean isAvailable(MetricValue m)
+	{
+		return ( (m != MetricValue.NONE) && getAvailable(m) && (m.value != 0.0) );
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Returns the actual value if available.
+	 ************************************************************************/
+
+	public static float getValue(MetricValue m)
+	{
+		return m.getValue();
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Makes the given actual value available.
+	 ************************************************************************/
+
+	public static void setValue(MetricValue m, double value)
+	{
+		setAvailable(m, true);
+		m.value = (float) value;
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Returns whether the annotation value is available.
+	 ************************************************************************/
+
+	public static boolean isAnnotationAvailable(MetricValue m)
+	{
+		return (m != MetricValue.NONE) && getAnnotationAvailable(m);
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Returns the annotation value if available.
+	 ************************************************************************/
+
+	public static float getAnnotationValue(MetricValue m)
+	{
+		return m.annotation;
+	}
+
+
+
+
+	/*************************************************************************
+	 *	Makes the given annotation value available.
+	 ************************************************************************/
+
+	public static void setAnnotationValue(MetricValue m, double annotation)
+	{
+		setAnnotationAvailable(m, true);
+		m.annotation = (float) annotation;
+	}
+
+
+	public static void setAnnotationValue(MetricValue m, float annotation)
+	{
+		setAnnotationAvailable(m, true);
+		m.annotation = annotation;
+	}
+
+
+	public static boolean isZero(MetricValue m) 
+	{
+		if (m != MetricValue.NONE) {
+			return ( Double.compare(0.0, m.value) == 0 );
+		}
+		return true;
+	}
+
+	/*************************************************************************
+	 *	Compares the metric value to another one.
+	 *
+	 *	Unavailable or nonexistent values are treated as less than available values.
+	 *	Annotation values are ignored.
+	 *
+	 ************************************************************************/
+
+	public static int compareTo(MetricValue left, MetricValue right)
+	{
+		int result;
+
+		if( MetricValue.isAvailable(left) && MetricValue.isAvailable(right) )
+		{
+			if( left.value > right.value )
+				result = +1;
+			else if( left.value < right.value )
+				result = -1;
+			else
+				result = 0;
+		}
+		else if( MetricValue.isAvailable(left) )
 			result = +1;
-		else if( left.value < right.value )
+		else if( MetricValue.isAvailable(right) )
 			result = -1;
 		else
 			result = 0;
+
+		return result;
 	}
-	else if( MetricValue.isAvailable(left) )
-		result = +1;
-	else if( MetricValue.isAvailable(right) )
-		result = -1;
-	else
-		result = 0;
-		
-	return result;
-}
 
 
 

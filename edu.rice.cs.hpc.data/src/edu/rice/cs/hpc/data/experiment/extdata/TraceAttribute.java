7d7dda16afc693a100cd66ea2dc2532e61da81b0
ptaffet <ptaffet@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-06-15 13:41:21 +0000

    New branch for Summer 2012

--- edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/extdata/TraceAttribute.java
+++
@@ -1,8 +0,0 @@
-package edu.rice.cs.hpc.data.experiment.extdata;
-
-public class TraceAttribute {
-	public String dbGlob;
-	public long dbTimeMin;
-	public long dbTimeMax;
-	public int dbHeaderSize;
-}

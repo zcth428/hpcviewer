df64251f8ea7420dad9c09795135518e5afcf02d
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-07-06 16:41:05 +0000

    Add action to filter ranks

--- edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/actions/FilterRanks.java
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/actions/FilterRanks.java
@@ -3,21 +3,58 @@ package edu.rice.cs.hpc.traceviewer.actions;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.services.ISourceProviderService;
 
-import edu.rice.cs.hpc.traceviewer.util.FilterRankDialog;
+import edu.rice.cs.hpc.traceviewer.filter.FilterTimeline;
+import edu.rice.cs.hpc.traceviewer.filter.FilterDialog;
+import edu.rice.cs.hpc.traceviewer.services.DataService;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
+import edu.rice.cs.hpc.traceviewer.timeline.ITimeline;
 
+
+/******
+ * 
+ * An action to filter ranks 
+ *
+ */
 public class FilterRanks extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
 		final Shell shell = HandlerUtil.getActiveShell(event);
-		final FilterRankDialog filterDlg = new FilterRankDialog(shell);
+		IWorkbenchWindow winObj = HandlerUtil.getActiveWorkbenchWindow(event);
+		ISourceProviderService sourceProviderService = (ISourceProviderService) winObj.getService(
+				ISourceProviderService.class);
 		
-		final int ret = filterDlg.open();
+		DataService dataService = (DataService) sourceProviderService.getSourceProvider(DataService.DATA_PROVIDER);
+
+		final SpaceTimeData data = dataService.getData();
+		FilterTimeline filter;
 		
-		return null;
+		// get the existing filter if exist
+		ITimeline timeline = data.getTimeline();
+		if (timeline instanceof FilterTimeline) {
+			// filter exists
+			filter = (FilterTimeline) timeline;
+		} else {
+			// the filter doesn't exist, we need to create a new one
+			filter = new FilterTimeline();
+		}
+		
+		FilterDialog dlg = new FilterDialog(shell, filter);
+		
+		if (dlg.open() == Dialog.OK) {
+			filter.setShownMode(false);
+			filter.filter(data.getProcessNames());
+			data.setTimeline(filter);
+			dataService.broadcastUpdate(data);
+		}
+		
+		return null; 
 	}
 
 }

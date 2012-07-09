43c0395a5911f2fe418889d1bd79c2342f52b1e0
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-07-09 16:21:02 +0000

    Reverse back to the original rev. 1319. Instead of adding filter abstraction in traceviewer which is error-prone, we will add a new layer in hpcdata instead.

--- edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/filter/IFilterTimeline.java
+++
@@ -1,8 +0,0 @@
-package edu.rice.cs.hpc.traceviewer.filter;
-
-import edu.rice.cs.hpc.traceviewer.timeline.ITimeline;
-
-public interface IFilterTimeline extends ITimeline {
-
-	public void filter( String []data) ;
-}

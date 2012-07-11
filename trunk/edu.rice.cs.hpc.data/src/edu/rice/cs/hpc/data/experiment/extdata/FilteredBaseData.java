71578913be519cb6e689f4d2865781b278bfc533
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-07-11 20:16:31 +0000

    refactoring BaseData and FilteredBaseData to avoid redundant code.

--- trunk/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/extdata/FilteredBaseData.java
+++ trunk/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/extdata/FilteredBaseData.java
@@ -15,13 +15,11 @@ import java.util.TreeMap;
  * @see IBaseData
  * @see BaseDataFile
  *******************************************************************/
-public class FilteredBaseData implements IBaseData {
+public class FilteredBaseData extends AbstractBaseData implements IBaseData {
 
-	final private BaseDataFile baseDataFile;
 	private Filter filter;
 	private String []filteredRanks;
 	private int []indexes;
-	final int headerSize;
 
 	/*****
 	 * construct a filtered data
@@ -33,8 +31,7 @@ public class FilteredBaseData implements IBaseData {
 	 */
 	public FilteredBaseData(String filename, int headerSize) throws IOException 
 	{
-		baseDataFile = new BaseDataFile(filename);
-		this.headerSize = headerSize;
+		super( filename, headerSize);
 	}
 
 	/****
@@ -123,13 +120,6 @@ public class FilteredBaseData implements IBaseData {
 		return filter;
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getHeaderSize()
-	 */
-	public int getHeaderSize() {
-		return headerSize;
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -155,45 +145,6 @@ public class FilteredBaseData implements IBaseData {
 		return indexes.length;
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getOffsets()
-	 */
-	public long[] getOffsets() {
-		return baseDataFile.getOffsets();
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getString(long)
-	 */
-	public String getString(long position, long length) {
-		return baseDataFile.getMasterBuffer().getString(position, length);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getLong(long)
-	 */
-	public long getLong(long position) {
-		return baseDataFile.getMasterBuffer().getLong(position);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getInt(long)
-	 */
-	public int getInt(long position) {
-		return baseDataFile.getMasterBuffer().getInt(position);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see edu.rice.cs.hpc.data.experiment.extdata.IBaseData#getDouble(long)
-	 */
-	public double getDouble(long position) {
-		return baseDataFile.getMasterBuffer().getDouble(position);
-	}
 
 	/*
 	 * (non-Javadoc)

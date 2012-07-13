fb55b1169bc7298da57fe9e3707b4b79ef07a729
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-07-13 20:10:22 +0000

    Add unit test for merging data files

---
+++ tests/UnitTests/src/edu/rice/cs/hpc/test/experiment/extdata/MergeDataFilesTest.java
@@ -0,0 +1,49 @@
+package edu.rice.cs.hpc.test.experiment.extdata;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.rice.cs.hpc.data.util.IProgressReport;
+import edu.rice.cs.hpc.data.util.MergeDataFiles;
+
+import junit.framework.TestCase;
+
+/*****
+ * 
+ * testing merging files
+ *
+ */
+public class MergeDataFilesTest extends TestCase {
+	
+	public void testMerge() {
+		
+		final String dir = "data/gauss/";
+		File file = new File(dir);
+		assertTrue ("Directory is not readable: " + dir,file.isDirectory());
+
+		try {
+			final String outputFile = "experiment.mt";
+			IProgressReport progress = new IProgressReport(){
+
+				public void begin(String title, int num_tasks) {
+					System.out.println("start: " + title);
+				}
+
+				public void advance() {	System.out.print(".");}
+
+				public void end() {
+					System.out.println("\nDone.");
+				}
+				
+			};
+			final MergeDataFiles.MergeDataAttribute att = MergeDataFiles.merge(file, "*.hpctrace", outputFile, progress);
+			
+			assertNotNull("Fail to merge: " + dir,att);
+			
+		} catch (IOException e) {
+			assertFalse("Unable to merge: " + dir, false);
+			e.printStackTrace();
+		}
+	}
+
+}

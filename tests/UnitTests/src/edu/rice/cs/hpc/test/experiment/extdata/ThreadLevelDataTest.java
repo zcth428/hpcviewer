9233d6799e22f95dc14a273e2e8a2b500449d97f
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-07-16 18:03:44 +0000

    Unit test for reading the header, location and offset

--- tests/UnitTests/src/edu/rice/cs/hpc/test/experiment/extdata/ThreadLevelDataTest.java
+++ tests/UnitTests/src/edu/rice/cs/hpc/test/experiment/extdata/ThreadLevelDataTest.java
@@ -46,9 +46,29 @@ public class ThreadLevelDataTest extends TestCase {
 	}
 	
 	@Test	
-	public void testRanks() {		
+	public void testRanks() {
+		// the number of ranks should match
+		final String[] ranks = dataTrace.getListOfRanks();
+		
 		assertTrue(dataTrace.getListOfRanks().length == dataTrace.getNumberOfRanks());
 		assertTrue(4 == dataTrace.getNumberOfRanks());
+		
+		// the name of the ranks should match
+		assertEquals("0", ranks[0]);
+		assertEquals("1", ranks[1]);
+		assertEquals("2", ranks[2]);
+		assertEquals("3", ranks[3]);
 	}
 	
+	@Test
+	public void testLocation() {
+		long l0 = dataTrace.getMinLoc(0);
+		assertTrue(96 == l0);
+		
+		int headerSize = dataTrace.getHeaderSize();
+		assertTrue(24 == headerSize);
+		
+		long l1 = dataTrace.getMaxLoc(0, headerSize);
+		assertTrue(106416 == l1);
+	}
 }

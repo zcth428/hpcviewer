568b7395725b12e8b63f231899c6c8ce9f0d2880
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-08-07 16:23:27 +0000

    Release 5.3.0 for hpcviewer and hpctraceviewer The release has several known bugs, which will be handled in the next release:
    in hpcviewer:
    - increase/decrease font
    - non adjustable row height on some OS 
    - not possible to mix custom format with augmented metric percent
    
    in hpctraceviewer:
    - inconsistency in undo/redo operations
    - inconsistency when changing time range in depth view
    - white pixels in the middle of the stack in the depth view

---
+++ tags/hpcviewer-5.3.0-release/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/db/FileData.java
@@ -0,0 +1,13 @@
+package edu.rice.cs.hpc.traceviewer.db;
+
+import java.io.File;
+
+/***
+ * 
+ * Class to store the location of both the XML and trace files
+ *
+ */
+public class FileData {
+	public File fileXML;
+	public File fileTrace;
+}

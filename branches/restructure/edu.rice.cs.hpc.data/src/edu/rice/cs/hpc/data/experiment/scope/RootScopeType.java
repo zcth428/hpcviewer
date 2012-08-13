91073becb927725973f01e2a4ce07da81ed88cb6
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-08-13 21:37:36 +0000

    Remove parasite comments.

--- branches/restructure/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/RootScopeType.java
+++ branches/restructure/edu.rice.cs.hpc.data/src/edu/rice/cs/hpc/data/experiment/scope/RootScopeType.java
@@ -1,20 +1,5 @@
 package edu.rice.cs.hpc.data.experiment.scope;
 
-/*public enum ScopeVisitType {
-	PreVisit, PostVisit
-}*/
-
-/*public class ScopeVisitType {
-	
-	public final static int PreVisit = 0;
-	public final static int PostVisit = 1;
-	
-	public int value;
-	
-	public ScopeVisitType(int v) { value = v; };
-	public boolean isPreVisit() { return value == PreVisit; }
-	public boolean isPostVisit() { return value == PostVisit; }
-}*/
 
 public class RootScopeType {
 	public final static RootScopeType Flat = new RootScopeType("Flat");

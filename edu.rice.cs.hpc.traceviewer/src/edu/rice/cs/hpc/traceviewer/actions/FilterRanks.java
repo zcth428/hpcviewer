7470efbdab3bbfa126830dfba418a32543c12108
laksono <laksono@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2012-08-27 21:46:58 +0000

    hpcviewer and hpctraceviewer 5.3.1 Release
    New Features hpcviewer:
    
        Add a feature to customize metric format display
        Add a feature to mix between flattening and zooming a node in flat view 
    
    Bug fixes
    
        hpctraceviewer: Eliminate memory leaks especially on Windows
        hpcviewer: Fix bug when closing hpcviewer window

---
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/actions/FilterRanks.java
@@ -0,0 +1,69 @@
+package edu.rice.cs.hpc.traceviewer.actions;
+
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.services.ISourceProviderService;
+
+import edu.rice.cs.hpc.data.experiment.extdata.FilteredBaseData;
+import edu.rice.cs.hpc.data.experiment.extdata.IBaseData;
+import edu.rice.cs.hpc.data.experiment.extdata.Filter;
+import edu.rice.cs.hpc.traceviewer.filter.FilterDialog;
+import edu.rice.cs.hpc.traceviewer.services.DataService;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeData;
+
+/*****
+ * 
+ * Action class to filter ranks
+ *
+ */
+public class FilterRanks extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		final Shell shell = HandlerUtil.getActiveShell(event);
+		IWorkbenchWindow winObj = HandlerUtil.getActiveWorkbenchWindow(event);
+		ISourceProviderService sourceProviderService = (ISourceProviderService) winObj.getService(
+				ISourceProviderService.class);
+		
+		DataService dataService = (DataService) sourceProviderService.getSourceProvider(DataService.DATA_PROVIDER);
+
+		final SpaceTimeData data = dataService.getData();
+		IBaseData baseData = data.getBaseData();
+		
+		Filter filter;
+		if (baseData instanceof FilteredBaseData) {
+			filter = ((FilteredBaseData)baseData).getFilter();
+		} else {
+			filter = new Filter();
+		}
+	
+		try {
+			
+			FilteredBaseData filteredBaseData = new FilteredBaseData(data.getTraceFile().getAbsolutePath(), 
+					data.getTraceAttribute().dbHeaderSize, 24);
+			
+			filteredBaseData.setFilter( filter );
+			
+			FilterDialog dlgFilter = new FilterDialog(shell, filteredBaseData);
+			if (dlgFilter.open() == Dialog.OK) {
+				
+				data.setBaseData(filteredBaseData);
+				dataService.broadcastUpdate(new Boolean(true));
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		return null;
+	}
+
+}
---
+++ edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/util/Constants.java
@@ -0,0 +1,22 @@
+package edu.rice.cs.hpc.traceviewer.util;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+public class Constants {
+	
+	public final static Color COLOR_WHITE = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE);
+    public final static Color COLOR_BLACK = Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
+    
+	/**The darkest color for black over depth text (switch to white if the sum of the 
+	 * R, G, and B components is less than this number).*/
+	public final static short DARKEST_COLOR_FOR_BLACK_TEXT = 384;
+	
+	/**The min number of time units you can zoom in*/
+	public final static int MIN_TIME_UNITS_DISP = 1;
+	
+	public static final int dataIdxNULL = -1;
+	public static final int dataIdxNoData = 0; // corresponds to "no data"
+	public static final String dataStrNULL = null;
+}

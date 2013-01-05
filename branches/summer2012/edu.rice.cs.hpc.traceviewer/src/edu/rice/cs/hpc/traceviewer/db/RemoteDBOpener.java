dcef8e3c741685b1f40685d07e0ab4567b89708f
ptaffet <ptaffet@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2013-01-05 03:27:49 +0000

    Extracted magic constants, about to merge into trunk.

--- branches/summer2012/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/db/RemoteDBOpener.java
+++ branches/summer2012/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/db/RemoteDBOpener.java
@@ -26,7 +26,13 @@ import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeDataController;
 import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeDataControllerRemote;
 
 public class RemoteDBOpener extends AbstractDBOpener {
-
+	//For more information on message structure, see protocol documentation at the end of RemoteDataReceiver 
+	private static final int DONE = 0x444F4E45;
+	private static final int OPEN = 0x4F50454E;
+	private static final int INFO = 0x494E464F;
+	private static final int XML_HEADER = 0x45584D4C;
+	private static final int DATABASE_NOT_FOUND = 0x4E4F4442;
+	private static final int DB_OK = 0x44424F4B;
 	DataOutputStream sender;
 	DataInputStream receiver;
 
@@ -34,12 +40,7 @@ public class RemoteDBOpener extends AbstractDBOpener {
 	@Override
 	SpaceTimeDataController openDBAndCreateSTDC(IWorkbenchWindow window,
 			String[] args, IStatusLineManager statusMgr) {
-		//System.out.println("OpenDB called");
-		/*String serverURL = "localhost";
-		//serverURL = "les.cs.rice.edu";
-		int port = 21590;
-		String serverPathToDB = "/Users/pat2/Downloads/hpctoolkit-chombo-crayxe6-1024pe-trace/";
-		//serverPathToDB = "/home/pat2/database";*/
+
 		String serverURL = args[0];
 		int port = Integer.parseInt(args[1]);
 		String serverPathToDB = args[2];
@@ -85,7 +86,7 @@ public class RemoteDBOpener extends AbstractDBOpener {
 			int XMLMessagePortNumber;
 			int CompressionType;
 			String[] ValuesX;
-			if (Message == 0x44424F4B)// DBOK
+			if (Message == DB_OK)// DBOK
 			{
 				XMLMessagePortNumber  = receiver.readInt();
 				traceCount = receiver.readInt();
@@ -101,7 +102,7 @@ public class RemoteDBOpener extends AbstractDBOpener {
 					}
 				}
 				
-			} else if (Message == 0x4E4F4442)// NODB
+			} else if (Message == DATABASE_NOT_FOUND)// NODB
 			{
 				// Tell the user
 				// TODO: Implement some UI notification
@@ -120,8 +121,8 @@ public class RemoteDBOpener extends AbstractDBOpener {
 			if (XMLMessagePortNumber == port)
 			{
 				int exml = receiver.readInt();
-				if (exml != 0x45584D4C)
-				System.out.println("Expected XML Message (" +0x45584D4C +")  on data socket, got " + exml);
+				if (exml != XML_HEADER)
+					System.out.println("Expected XML Message (" +XML_HEADER +")  on data socket, got " + exml);
 				int size = receiver.readInt();
 				CompressedXMLMessage = new byte[size];
 				int numRead = 0;
@@ -179,7 +180,7 @@ public class RemoteDBOpener extends AbstractDBOpener {
 		/*
 		 * Then: overallMinTime (long) overallMaxTime (long) headerSize (int)
 		 */
-		sender.writeInt(0x494E464F);// INFO
+		sender.writeInt(INFO);
 		sender.writeLong(stData.getMinBegTime());
 		sender.writeLong(stData.getMaxEndTime());
 		sender.writeInt(stData.HEADER_SIZE);
@@ -188,7 +189,7 @@ public class RemoteDBOpener extends AbstractDBOpener {
 	}
 
 	private void sendOpenDB(String serverPathToDB) throws IOException {
-		sender.writeInt(0x4F50454E);// "OPEN" in ascii
+		sender.writeInt(OPEN);
 		int len = serverPathToDB.length();
 		sender.writeShort(len);
 		for (int i = 0; i < len; i++) {
@@ -207,12 +208,12 @@ public class RemoteDBOpener extends AbstractDBOpener {
 	void closeDB() {
 		try {
 			DataOutputStream closer = new DataOutputStream(serverConnection.getOutputStream());
-			closer.writeInt(0x444F4E45);
+			closer.writeInt(DONE);
 			closer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}// DONE in ASCII
+		}
 
 	}
 

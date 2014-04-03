e6498f37f1316527d9dd68918f09716df2db0cd8
laksono@gmail.com <laksono@gmail.com@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2014-04-03 21:02:12 +0000

    copy manually the traceviewer's data plugin from Brett's branch to the main trunk. It seems Eclipse SVN doesn't know how to a new plugin from another branch to the current repository since Eclipse workspace is based on project, not directory. If in the current workspace the project to copy doesn't exist, then Eclipse gives up.

---
+++ edu.rice.cs.hpc.traceviewer.data/src/edu/rice/cs/hpc/traceviewer/data/db/DetailDataPreparation.java
@@ -0,0 +1,50 @@
+package edu.rice.cs.hpc.traceviewer.data.db;
+
+import org.eclipse.swt.graphics.Color;
+
+import edu.rice.cs.hpc.traceviewer.data.graph.ColorTable;
+import edu.rice.cs.hpc.traceviewer.data.timeline.ProcessTimeline;
+
+public class DetailDataPreparation extends DataPreparation {
+
+	private TimelineDataSet dataset;
+	
+	/*****
+	 * Constructor for preparing data to paint on the space-time canvas
+	 * 
+	 * @param _colorTable
+	 * @param _ptl
+	 * @param _begTime
+	 * @param _depth
+	 * @param _height
+	 * @param _pixelLength
+	 * @param _usingMidpoint
+	 */
+	public DetailDataPreparation(ColorTable _colorTable, ProcessTimeline _ptl,
+			long _begTime, int _depth, int _height, double _pixelLength,
+			boolean _usingMidpoint) 
+	{
+		super(_colorTable, _ptl, _begTime, _depth, _height, _pixelLength,
+				_usingMidpoint);
+		dataset = new TimelineDataSet( ptl.line(),_ptl.size(), height);
+	}
+
+	@Override
+	public void finishLine(int currSampleMidpoint, int succSampleMidpoint,
+			int currDepth, Color color, int sampleCount) {
+
+		final DetailDataVisualization data = new DetailDataVisualization(currSampleMidpoint, 
+				succSampleMidpoint, currDepth, color, sampleCount);
+		
+		dataset.add(data);
+	}
+
+	/*****
+	 * retrieve the list of data to paint
+	 * @return
+	 */
+	public TimelineDataSet getList() {
+		
+		return dataset;
+	}
+}

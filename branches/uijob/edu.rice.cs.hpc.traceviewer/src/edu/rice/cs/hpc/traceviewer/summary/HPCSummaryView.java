8eeb3c4e08e9f8d483c0a5ff7e7a181165894cba
laksono@gmail.com <laksono@gmail.com@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2014-06-07 08:02:45 +0000

    Create a branch to work with Eclipse's UI job instead of using our own progress bar. To use Eclipse's job, we need to redesign the classes which may cause code instability.
    The advantage of Eclipse UIJob is it's maintained by Eclipse and (mostly) guaranteed to be compatible with future releases. Plus it enables users to cancel the current processing job.

---
+++ branches/uijob/edu.rice.cs.hpc.traceviewer/src/edu/rice/cs/hpc/traceviewer/summary/HPCSummaryView.java
@@ -0,0 +1,82 @@
+package edu.rice.cs.hpc.traceviewer.summary;
+
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISourceProvider;
+import org.eclipse.ui.ISourceProviderListener;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.services.ISourceProviderService;
+
+import edu.rice.cs.hpc.traceviewer.services.DataService;
+import edu.rice.cs.hpc.traceviewer.spaceTimeData.SpaceTimeDataController;
+
+/*************************************************************************
+ * 
+ * View part of the summary window 
+ *
+ *************************************************************************/
+public class HPCSummaryView extends ViewPart
+{
+
+	public static final String ID = "hpcsummaryview.view";
+	
+	/**The canvas that actually displays this view*/
+	SummaryTimeCanvas summaryCanvas;
+	
+	public void createPartControl(Composite master)
+	{
+		/*************************************************************************
+		 * Master Composite
+		 */
+		
+		master.setLayout(new GridLayout());
+		master.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		/*************************************************************************
+		 * Summary View Canvas
+		 */
+		
+		summaryCanvas = new SummaryTimeCanvas(master);
+		summaryCanvas.setLayout(new GridLayout());
+		summaryCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		summaryCanvas.setVisible(false);
+		
+		setListener();
+	}
+	
+	private void setListener() {
+		ISourceProviderService service = (ISourceProviderService)getSite().getService(ISourceProviderService.class);
+		ISourceProvider serviceProvider = service.getSourceProvider(DataService.DATA_UPDATE);
+		serviceProvider.addSourceProviderListener( new ISourceProviderListener(){
+
+			public void sourceChanged(int sourcePriority, Map sourceValuesByName) {	}
+			public void sourceChanged(int sourcePriority, String sourceName,
+					Object sourceValue) {
+				// eclipse bug: even if we set a very specific source provider, eclipse still
+				//	gather event from other source. we then require to put a guard to avoid this.
+				if (sourceName.equals(DataService.DATA_UPDATE)) {
+					summaryCanvas.redraw();
+				}
+			}
+		});
+	}
+
+	public void updateView(SpaceTimeDataController dataTraces)
+	{
+		summaryCanvas.updateData(dataTraces);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
+	public void setFocus() 
+	{
+		summaryCanvas.setFocus();
+	}
+
+}
